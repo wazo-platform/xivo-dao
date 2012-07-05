79e4bcf143bdb858f268e752172b64d6fded675d
Cedric Abunar <cabunar@avencall.com>
2012-07-05 14:34:19 -0400

    add method "add_full_call" in call_queue_dao

--- xivo-dao/xivo_dao/call_on_queue_dao.py
+++ xivo-dao/xivo_dao/call_on_queue_dao.py
@@ -1,5 +1,22 @@
 # -*- coding: UTF-8 -*-
 
+from xivo_dao.alchemy.call_on_queue import CallOnQueue
+from xivo_dao.alchemy import dbconnection
 
-def add_full_call(callid, time, queue_name):
-    pass
+_DB_NAME = 'asterisk'
+
+
+def _session():
+    connection = dbconnection.get_connection(_DB_NAME)
+    return connection.get_session()
+
+
+def add_full_call(callid, time, queue_id):
+        call_on_queue = CallOnQueue()
+        call_on_queue.time = time
+        call_on_queue.callid = callid
+        call_on_queue.queue_id = queue_id
+        call_on_queue.status = 'full'
+
+        _session().add(call_on_queue)
+        _session().commit()
