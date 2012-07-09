57a73dffe11e93bf42f161994cbb2898f2d1ecfe
Cedric Abunar <cabunar@avencall.com>
2012-07-09 13:14:40 -0400

    [evol]add Makefile and setup.py for packaging

---
+++ xivo-dao/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='xivo-dao',
+    version='0.1',
+    description='XiVO Data Access Object',
+    author='Avencall',
+    author_email='dev@avencall.com',
+    url='http://git.xivo.fr/',
+    license='GPLv3',
+    packages=['xivo_dao',
+              'xivo_dao.alchemy',
+              'xivo_dao.helpers'],
+)
