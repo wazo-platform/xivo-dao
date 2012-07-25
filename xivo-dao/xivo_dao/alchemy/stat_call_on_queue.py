c8f203ada27b21c3c7e85d2f186e7efc34d97cf2
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-07-25 15:57:43 -0400

    [fix] forgot the import

--- xivo-dao/xivo_dao/alchemy/stat_call_on_queue.py
+++ xivo-dao/xivo_dao/alchemy/stat_call_on_queue.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.schema import Column, ForeignKey, Sequence
 from sqlalchemy.types import String, TIMESTAMP, Integer, Enum
 from xivo_dao.alchemy.base import Base, Type
 
