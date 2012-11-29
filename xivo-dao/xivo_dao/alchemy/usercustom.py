316234f3f9915ee4ac91b810f75fd1a773393151
Cedric Abunar <cabunar@avencall.com>
2012-11-29 18:06:39 -0500

    [clean]remove unued import

--- xivo-dao/xivo_dao/alchemy/usercustom.py
+++ xivo-dao/xivo_dao/alchemy/usercustom.py
@@ -23,7 +23,7 @@
 
 from xivo_dao.alchemy.base import Base
 from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, String, Text
+from sqlalchemy.types import Integer, String
 
 
 class UserCustom(Base):
