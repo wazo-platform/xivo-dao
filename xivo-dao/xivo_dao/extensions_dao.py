1a8e548117b428e6e48065615f33915601ae4ad4
Cedric Abunar <cabunar@avencall.com>
2012-12-07 14:42:13 -0500

    [refactor]normalize filename for extensions_dao

--- xivo-dao/xivo_dao/extensions_dao.py
+++ xivo-dao/xivo_dao/extensions_dao.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-# vim: set fileencoding=utf-8 :
+# -*- coding: utf-8 -*-
 
-# Copyright (C) 2007-2011  Avencall
+# Copyright (C) 2007-2012  Avencall
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -9,9 +8,9 @@
 # (at your option) any later version.
 #
 # Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Pro-formatique SARL. See the LICENSE file at top of the
-# source tree or delivered in the installable package in which XiVO CTI Server
-# is distributed for more details.
+# contracted with Avencall. See the LICENSE file at top of the source tree
+# or delivered in the installable package in which XiVO CTI Server is
+# distributed for more details.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
--- xivo-dao/xivo_dao/tests/test_extensions_dao.py
+++ xivo-dao/xivo_dao/tests/test_extensions_dao.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# vim: set fileencoding=utf-8 :
+# -*- coding: utf-8 -*-
 
 # Copyright (C) 2007-2012  Avencall
 #
@@ -9,9 +8,9 @@
 # (at your option) any later version.
 #
 # Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Pro-formatique SARL. See the LICENSE file at top of the
-# source tree or delivered in the installable package in which XiVO CTI Server
-# is distributed for more details.
+# contracted with Avencall. See the LICENSE file at top of the source tree
+# or delivered in the installable package in which XiVO CTI Server is
+# distributed for more details.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from xivo_dao import extensionsdao
+from xivo_dao import extensions_dao
 from xivo_dao.alchemy.extension import Extension
 from xivo_dao.tests.test_dao import DAOTestCase
 
@@ -46,8 +45,8 @@ class TestExtensionsDAO(DAOTestCase):
         self.session.commit()
 
     def test_exten_by_name(self):
-        enablednd = extensionsdao.exten_by_name('enablednd')
-        phoneprogfunckey = extensionsdao.exten_by_name('phoneprogfunckey')
+        enablednd = extensions_dao.exten_by_name('enablednd')
+        phoneprogfunckey = extensions_dao.exten_by_name('phoneprogfunckey')
 
         self.assertEqual(enablednd, '*25')
         self.assertEqual(phoneprogfunckey, '_*735')
