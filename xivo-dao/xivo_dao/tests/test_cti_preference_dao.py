495ca12e8b6d8d333c9787dac176582afac9cac8
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-01-22 16:31:50 -0500

    adjust license in python file headers
    Change-Id: I0ecfb1ed14434eaa71d0d7ff6d1f6e807e06ae0d

--- xivo-dao/xivo_dao/tests/test_cti_preference_dao.py
+++ xivo-dao/xivo_dao/tests/test_cti_preference_dao.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012  Avencall
+
+# Copyright (C) 2012-2013 Avencall
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Avencall. See the LICENSE file at top of the source tree
-# or delivered in the installable package in which XiVO CTI Server is
-# distributed for more details.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import unittest
 
