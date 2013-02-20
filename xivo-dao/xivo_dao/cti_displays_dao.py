ae482d08e2ca68b0f3a90e75bac9a9134837a9e8
Cedric Abunar <cabunar@avencall.com>
2013-02-20 16:30:39 -0500

    new dao for ctidisplay config

---
+++ xivo-dao/xivo_dao/cti_displays_dao.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012-2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+import json
+from xivo_dao.alchemy.cti_displays import CtiDisplays
+from xivo_dao.helpers.db_manager import daosession
+
+
+@daosession
+def get_config(session):
+    res = {}
+    rows = (session.query(CtiDisplays).all())
+    for row in rows:
+        res[row.name] = json.loads(row.data)
+    return res
