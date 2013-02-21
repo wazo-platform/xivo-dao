0912942c5926d7ac11a2f0c245d95510ae28d1ce
Cedric Abunar <cabunar@avencall.com>
2013-02-21 08:20:47 -0500

    new dao for ctiphonehints

---
+++ xivo-dao/xivo_dao/cti_phonehints_dao.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012-2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+from xivo_dao.helpers.db_manager import daosession
+from xivo_dao.alchemy.ctiphonehintsgroup import CtiPhoneHintsGroup
+from xivo_dao.alchemy.ctiphonehints import CtiPhoneHints
+
+
+@daosession
+def get_config(session):
+    res = {}
+    hintsgroups = (session.query(CtiPhoneHintsGroup).all())
+    for hintsgroup in hintsgroups:
+        res[hintsgroup.name] = {}
+        hints = get_hints_with_group_id(hintsgroup.id)
+        for hint in hints:
+            res[hintsgroup.name][hint.number] = {}
+            res[hintsgroup.name][hint.number]['longname'] = hint.name
+            res[hintsgroup.name][hint.number]['color'] = hint.color
+    return res
+
+
+@daosession
+def get_hints_with_group_id(session, group_id):
+    return session.query(CtiPhoneHints).filter(CtiPhoneHints.idgroup == group_id).all()
