ed46a2872d3c0d210b41112ad59e89f28ef8e813
Jean Aunis <jaunis@avencall.com>
2013-04-16 15:10:49 +0200

    added a dao for the rightcallmember table

---
+++ xivo-dao/xivo_dao/tests/test_rightcall_member_dao.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+from xivo_dao import rightcall_member_dao
+from xivo_dao.alchemy.rightcall import RightCall
+from xivo_dao.alchemy.rightcallmember import RightCallMember
+from xivo_dao.tests.test_dao import DAOTestCase
+
+class TestRightCallMemberDAO(DAOTestCase):
+
+    tables = [RightCall, RightCallMember]
+
+    def test_add_user_to_rightcall(self):
+        userid = 1
+        rightid = 2
+        rightcall_member_dao.add_user_to_rightcall(userid, rightid)
+
+        result = self.session.query(RightCallMember).first()
+        self.assertEquals('1', result.typeval)
+        self.assertEquals(2, result.rightcallid)
+        self.assertEquals('user', result.type)
