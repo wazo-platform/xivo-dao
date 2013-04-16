9dfba1e63d23b089b03aa4a48bea5d37dca37ed4
Jean Aunis <jaunis@avencall.com>
2013-04-16 12:01:11 +0200

    implemented 'add_user_to_queue' in queue_member_dao

--- xivo-dao/xivo_dao/queue_member_dao.py
+++ xivo-dao/xivo_dao/queue_member_dao.py
@@ -15,9 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from xivo_dao.alchemy.queuemember import QueueMember
 from sqlalchemy import func, select
+from xivo_dao import line_dao
 from xivo_dao.alchemy.queuefeatures import QueueFeatures
+from xivo_dao.alchemy.queuemember import QueueMember
 from xivo_dao.helpers.db_manager import daosession
 
 
@@ -76,3 +77,23 @@ def get_queue_members_for_queues(session):
             .filter(QueueMember.queue_name.in_(queue_name_subquery))
             .all())
     return rows
+
+@daosession
+def add_user_to_queue(session, user_id, line_id, queue):
+    next_position = _get_next_position_for_queue(session, queue)
+    queue_member = QueueMember()
+    queue_member.queue_name = queue
+    queue_member.interface = line_dao.get_interface_from_user_id(user_id)
+    queue_member.usertype = 'user'
+    queue_member.userid = user_id
+    queue_member.channel = queue_member.interface.split('/', 1)[0]
+    queue_member.category = 'queue'
+    queue_member.position = next_position
+
+    try:
+        session.begin()
+        session.add(queue_member)
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
