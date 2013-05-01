43437173a8f4efc1292ffddbf6a29b10cf3892f5
Sebastien Duthil <sduthil@avencall.com>
2013-05-01 15:39:23 -0400

    implement creation in voicemail service

--- xivo-dao/xivo_dao/services/tests/test_voicemail.py
+++ xivo-dao/xivo_dao/services/tests/test_voicemail.py
@@ -12,20 +12,132 @@ class TestVoicemail(unittest.TestCase):
         pass
 
     @patch('xivo_dao.services.voicemail.voicemail_dao')
-    def test_delete_by_number_raises_exception(self, voicemail_dao):
-        voicemail_dao.find_by_number.return_value = None
+    def test_delete_raises_exception(self, voicemail_dao):
+        voicemail_dao.find_voicemail.return_value = None
 
         self.assertRaises(voicemail_service.VoicemailNotFoundException,
-                          voicemail_service.delete_by_number,
-                          '42')
+                          voicemail_service.delete,
+                          '42', 'default')
 
     @patch('xivo_dao.services.voicemail.voicemail_dao')
-    def test_delete_by_number(self, voicemail_dao):
+    def test_delete(self, voicemail_dao):
+        number = '42'
+        context = 'default'
         voicemail = Mock(Voicemail)
 
-        voicemail_dao.find_by_number.return_value = voicemail
+        voicemail_dao.find_voicemail.return_value = voicemail
 
-        voicemail_service.delete_by_number('42')
+        voicemail_service.delete(number, context)
 
-        voicemail_dao.find_by_number.assert_called_once_with('42')
+        voicemail_dao.find_voicemail.assert_called_once_with(number, context)
         voicemail_dao.delete.assert_called_once_with(voicemail)
+
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    def test_create_no_properties(self, voicemail_dao):
+        voicemail = {}
+
+        self.assertRaises(voicemail_service.MissingParametersError, voicemail_service.create, voicemail)
+
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    @patch('xivo_dao.services.context.find_by_name')
+    def test_create_empty_name(self, context_find, voicemail_dao):
+        name = ''
+        number = '42'
+        context = 'default'
+        voicemail = {
+            'name': name,
+            'number': number,
+            'context': context,
+        }
+        context_find.return_value = Mock()
+        voicemail_dao.find_voicemail.return_value = None
+
+        self.assertRaises(voicemail_service.InvalidParametersError, voicemail_service.create, voicemail)
+
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    @patch('xivo_dao.services.context.find_by_name')
+    def test_create_invalid_number(self, context_find, voicemail_dao):
+        name = 'voicemail'
+        number = 'wrong_number'
+        context = 'default'
+        voicemail = {
+            'name': name,
+            'number': number,
+            'context': context,
+        }
+        context_find.return_value = Mock()
+        voicemail_dao.find_voicemail.return_value = None
+
+        self.assertRaises(voicemail_service.InvalidParametersError, voicemail_service.create, voicemail)
+
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    @patch('xivo_dao.services.context.find_by_name')
+    def test_create_invalid_context(self, context_find, voicemail_dao):
+        name = 'voicemail'
+        number = '42'
+        context = 'inexistant_context'
+        voicemail = {
+            'name': name,
+            'number': number,
+            'context': context,
+        }
+        context_find.return_value = None
+        voicemail_dao.find_voicemail.return_value = None
+
+        self.assertRaises(voicemail_service.InvalidParametersError, voicemail_service.create, voicemail)
+
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    @patch('xivo_dao.services.context.find_by_name')
+    def test_create_same_context_and_number(self, context_find, voicemail_dao):
+        name = 'voicemail'
+        number = '42'
+        context = 'existing_context'
+        voicemail = {
+            'name': name,
+            'number': number,
+            'context': context,
+        }
+        context_find.return_value = Mock()
+        voicemail_mock = Mock(Voicemail)
+        voicemail_mock.number = '42'
+        voicemail_mock.context = 'existing_context'
+        voicemail_dao.find_voicemail.return_value = voicemail_mock
+
+        self.assertRaises(voicemail_service.InvalidParametersError, voicemail_service.create, voicemail)
+
+    @patch('xivo_dao.services.context.find_by_name')
+    @patch('xivo_dao.services.voicemail.voicemail_dao')
+    @patch('xivo_dao.dao.voicemail.Voicemail.from_user_data')
+    def test_create(self, from_user_data_mock, voicemail_dao, context_find):
+        name = 'voicemail'
+        number = '42'
+        context = 'default'
+        voicemail = {
+            'name': name,
+            'number': number,
+            'context': context,
+        }
+        context_find.return_value = Mock()
+        voicemail_dao.find_voicemail.return_value = None
+        voicemail_mock_input = self._create_voicemail_mock(voicemail)
+        voicemail_mock_output = self._create_voicemail_mock(voicemail)
+        from_user_data_mock.return_value = voicemail_mock_input
+        voicemail_dao.create.return_value = voicemail_mock_output
+
+        result = voicemail_service.create(voicemail)
+
+        voicemail_dao.create.assert_called_once_with(voicemail_mock_input)
+        self._assert_voicemail_equals(voicemail_mock_output, result)
+
+    def _assert_voicemail_equals(self, voicemail_left, voicemail_right):
+        self.assertEquals(voicemail_left.name, voicemail_right.name)
+        self.assertEquals(voicemail_left.number, voicemail_right.number)
+        self.assertEquals(voicemail_left.context, voicemail_right.context)
+
+    def _create_voicemail_mock(self, properties):
+        mock = Mock(Voicemail)
+        mock.name = properties['name']
+        mock.number = properties['number']
+        mock.context = properties['context']
+
+        return mock
--- xivo-dao/xivo_dao/services/voicemail.py
+++ xivo-dao/xivo_dao/services/voicemail.py
@@ -1,22 +1,62 @@
 from xivo_dao.dao import voicemail as voicemail_dao
+from xivo_dao.dao.voicemail import Voicemail
+from xivo_dao.services import context as context_service
 
 
 class VoicemailNotFoundException(LookupError):
 
     @classmethod
-    def from_number(cls, number):
-        message = "Voicemail with number %s does not exist" % number
+    def from_number_and_context(cls, number, context):
+        message = "Voicemail %s@%s does not exist" % (number, context)
         return cls(message)
 
 
-def delete(**kwargs):
-    if 'number' in kwargs:
-        delete_by_number(kwargs['number'])
+class MissingParametersError(ValueError):
+
+    def __init__(self, missing_parameters):
+        ValueError.__init__(self, "Missing parameters: %s" % ','.join(missing_parameters))
 
 
-def delete_by_number(number):
-    voicemail = voicemail_dao.find_by_number(number)
+class InvalidParametersError(ValueError):
+
+    def __init__(self, invalid_parameters):
+        ValueError.__init__(self, "Invalid parameters: %s" % ','.join(invalid_parameters))
+
+
+def delete(number, context):
+    voicemail = voicemail_dao.find_voicemail(number, context)
     if not voicemail:
-        raise VoicemailNotFoundException.from_number(number)
+        raise VoicemailNotFoundException.from_number_and_context(number, context)
 
     voicemail_dao.delete(voicemail)
+
+
+def create(parameters):
+    _check_missing_parameters(parameters)
+    _check_invalid_parameters(parameters)
+    voicemail = Voicemail.from_user_data(parameters)
+    return voicemail_dao.create(voicemail)
+
+
+def _check_missing_parameters(voicemail):
+    missing_parameters = []
+    mandatory_parameters = ['name', 'number', 'context']
+    for mandatory_parameter in mandatory_parameters:
+        if mandatory_parameter not in voicemail:
+            missing_parameters.append(mandatory_parameter)
+    if missing_parameters:
+        raise MissingParametersError(missing_parameters)
+
+
+def _check_invalid_parameters(voicemail):
+    invalid_parameters = []
+    if not voicemail['name']:
+        invalid_parameters.append('name')
+    if not voicemail['number'].isdigit():
+        invalid_parameters.append('number')
+    if not context_service.find_by_name(voicemail['context']):
+        invalid_parameters.append('context')
+    if voicemail_dao.find_voicemail(voicemail['number'], voicemail['context']):
+        invalid_parameters.append('number')
+    if invalid_parameters:
+        raise InvalidParametersError(invalid_parameters)
