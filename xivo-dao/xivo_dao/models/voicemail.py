f05b2d83a5ab6fe0cc3af1459ab351eb441b0974
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-05-16 08:42:16 -0400

    forgot to add models

---
+++ xivo-dao/xivo_dao/models/voicemail.py
@@ -0,0 +1,39 @@
+class Voicemail(object):
+
+    MANDATORY = ['name',
+                 'number',
+                 'context']
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.number = kwargs.get('number')
+        self.context = kwargs.get('context')
+        self.id = kwargs.get('id')
+
+    @classmethod
+    def from_data_source(cls, properties):
+        voicemail = cls()
+        voicemail.name = properties.fullname
+        voicemail.number = properties.mailbox
+        voicemail.context = properties.context
+        voicemail.id = properties.uniqueid
+        return voicemail
+
+    @classmethod
+    def from_user_data(cls, properties):
+        voicemail = cls(**properties)
+        return voicemail
+
+    @property
+    def number_at_context(self):
+        return '%s@%s' % (self.number, self.context)
+
+    def missing_parameters(self):
+        missing = []
+
+        for parameter in self.MANDATORY:
+            attribute = getattr(self, parameter)
+            if attribute is None:
+                missing.append(parameter)
+
+        return missing
