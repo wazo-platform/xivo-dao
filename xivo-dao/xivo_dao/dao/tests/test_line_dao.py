310be23e950352d7852a76ca1ccd6be0deff171b
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-05-30 08:41:47 -0400

    Add function get_line_by_user_id using the new style dao

---
+++ xivo-dao/xivo_dao/dao/tests/test_line_dao.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from hamcrest import assert_that
+from hamcrest.core import equal_to
+from xivo_dao.alchemy.linefeatures import LineFeatures as LineSchema
+from xivo_dao.dao import line_dao
+from xivo_dao.models.line import Line
+from xivo_dao.tests.test_dao import DAOTestCase
+
+
+class GetLineDao(DAOTestCase):
+
+    tables = [LineSchema]
+
+    def setUp(self):
+        self.empty_tables()
+
+    def test_get_by_user_id_no_line(self):
+        self.assertRaises(LookupError, line_dao.get_line_by_user_id, 666)
+
+    def test_get_by_user_id(self):
+        user_id = 123
+        properties = {
+            'iduserfeatures': user_id,
+            'number': '1001',
+            'context': 'notdefault',
+            'name': 'sdklfj',
+            'protocolid': 123,
+            'provisioningid': 543,
+        }
+        self.add_me(LineSchema(**properties))
+
+        expected_line = Line.from_user_data(properties)
+
+        line = line_dao.get_line_by_user_id(user_id)
+
+        assert_that(line, equal_to(expected_line), 'Get line returns the user\'s line')
+
+    def test_get_by_user_id_commented(self):
+        user_id = 123
+        properties = {
+            'iduserfeatures': user_id,
+            'number': '1001',
+            'context': 'notdefault',
+            'name': 'sdklfj',
+            'protocolid': 123,
+            'provisioningid': 543,
+            'commented': 1,
+        }
+        self.add_me(LineSchema(**properties))
+
+        self.assertRaises(LookupError, line_dao.get_line_by_user_id, user_id)
