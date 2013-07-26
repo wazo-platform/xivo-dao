b3bbfc840625ba35db21958ec78899a16f572afb
Cedric Abunar <cabunar@avencall.com>
2013-07-26 10:01:51 -0400

    add new dao user_line_extension

---
+++ xivo-dao/xivo_dao/data_handler/user_line_extension/dao.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from xivo_dao.alchemy.user_line import UserLine as ULESchema
+from xivo_dao.helpers.db_manager import daosession
+from xivo_dao.data_handler.exception import ElementNotExistsError, \
+    ElementCreationError, ElementDeletionError, ElementEditionError
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from model import UserLineExtension
+
+
+@daosession
+def get(session, ule_id):
+    res = _new_query(session).filter(ULESchema.id == ule_id).first()
+
+    if not res:
+        raise ElementNotExistsError('UserLineExtension', id=ule_id)
+
+    return UserLineExtension.from_data_source(res)
+
+
+@daosession
+def create(session, user_line_extension):
+    user_line_extension_row = user_line_extension.to_data_source(ULESchema)
+    session.begin()
+    session.add(user_line_extension_row)
+
+    try:
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise ElementCreationError('UserLineExtension', e)
+    except IntegrityError as e:
+        session.rollback()
+        raise ElementCreationError('UserLineExtension', e)
+
+    user_line_extension.id = user_line_extension_row.id
+
+    return user_line_extension
+
+
+@daosession
+def edit(session, user_line_extension):
+    session.begin()
+    nb_row_affected = (session.query(ULESchema)
+                       .filter(ULESchema.id == user_line_extension.id)
+                       .update(user_line_extension.to_data_dict()))
+
+    try:
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise ElementEditionError('UserLineExtension', e)
+
+    if nb_row_affected == 0:
+        raise ElementEditionError('UserLineExtension', 'user_line_extension_id %s not exsit' % user_line_extension.id)
+
+    return nb_row_affected
+
+
+@daosession
+def delete(session, user_line_extension):
+    session.begin()
+    try:
+        nb_row_affected = session.query(ULESchema).filter(ULESchema.id == user_line_extension.id).delete()
+        session.commit()
+    except SQLAlchemyError, e:
+        session.rollback()
+        raise ElementDeletionError('UserLineExtension', e)
+
+    if nb_row_affected == 0:
+        raise ElementDeletionError('UserLineExtension', 'user_line_extension_id %s not exsit' % user_line_extension.id)
+
+    return nb_row_affected
+
+
+def _new_query(session):
+    return session.query(ULESchema)
---
+++ xivo-dao/xivo_dao/data_handler/user_line_extension/services.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import dao
+import notifier
+
+from xivo_dao.data_handler.exception import MissingParametersError, \
+    InvalidParametersError, ElementNotExistsError
+from xivo_dao.data_handler.line import services as line_services
+from xivo_dao.data_handler.user import services as user_services
+from xivo_dao.data_handler.extension import services as extension_services
+
+
+def get(ule_id):
+    return dao.get(ule_id)
+
+
+def create(ule):
+    _validate(ule)
+    extension = dao.create(ule)
+    notifier.created(ule)
+    return extension
+
+
+def edit(ule):
+    _validate(ule)
+    dao.edit(ule)
+    notifier.edited(ule)
+
+
+def delete(ule):
+    dao.delete(ule)
+    _remove_user(ule)
+    _remove_line(ule)
+    _remove_exten(ule)
+    notifier.deleted(ule)
+
+
+def _validate(ule):
+    _check_missing_parameters(ule)
+    _check_invalid_parameters(ule)
+
+
+def _check_missing_parameters(ule):
+    missing = ule.missing_parameters()
+    if missing:
+        raise MissingParametersError(missing)
+
+
+def _check_invalid_parameters(ule_id):
+    invalid_parameters = []
+    if not isinstance(ule_id.user_id, int):
+        invalid_parameters.append('user_id must be integer')
+    if ule_id.user_id == 0:
+        invalid_parameters.append('user_id equal to 0')
+    if not isinstance(ule_id.line_id, int):
+        invalid_parameters.append('line_id must be integer')
+    if ule_id.line_id == 0:
+        invalid_parameters.append('line_id equal to 0')
+    if not isinstance(ule_id.extension_id, int):
+        invalid_parameters.append('extension_id must be integer')
+    if ule_id.extension_id == 0:
+        invalid_parameters.append('extension_id equal to 0')
+    if not isinstance(ule_id.main_line, bool):
+        invalid_parameters.append('main_line must be boolean')
+    if not isinstance(ule_id.main_user, bool):
+        invalid_parameters.append('main_user must be boolean')
+    if invalid_parameters:
+        raise InvalidParametersError(invalid_parameters)
+
+
+def _remove_user(ule):
+    try:
+        user = user_services.get(ule.user_id)
+        user_services.delete(user)
+    except ElementNotExistsError:
+        return
+
+
+def _remove_line(ule):
+    try:
+        line = line_services.get(ule.line_id)
+        line_services.delete(line)
+    except ElementNotExistsError:
+        return
+
+
+def _remove_exten(ule):
+    try:
+        extension = extension_services.get(ule.extension_id)
+        extension_services.delete(extension)
+    except ElementNotExistsError:
+        return
