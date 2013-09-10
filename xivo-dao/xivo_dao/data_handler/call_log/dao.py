04e8d0162d03dc1ed51b0c1e50e1d223579d8f38
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-09-10 16:50:44 -0400

    Mark processed cels when creating call_log

--- xivo-dao/xivo_dao/data_handler/call_log/dao.py
+++ xivo-dao/xivo_dao/data_handler/call_log/dao.py
@@ -17,6 +17,7 @@
 
 from sqlalchemy.exc import SQLAlchemyError
 from xivo_dao.alchemy.call_log import CallLog as CallLogSchema
+from xivo_dao.alchemy.cel import CEL as CELSchema
 from xivo_dao.data_handler.exception import ElementCreationError, ElementDeletionError
 from xivo_dao.helpers.db_manager import daosession
 from xivo_dao.data_handler.call_log.model import CallLog
@@ -47,13 +48,26 @@ def find_all_in_period(session, start, end):
 def create_all(session, call_logs):
     session.begin()
     for call_log in call_logs:
-        call_log_row = call_log.to_data_source(CallLogSchema)
-        session.add(call_log_row)
+        call_log_row = _create_call_log(session, call_log)
+        _link_call_log(session, call_log, call_log_row)
+
     try:
         session.commit()
     except SQLAlchemyError as e:
         session.rollback()
         raise ElementCreationError('CallLog', e)
+
+
+def _create_call_log(session, call_log):
+    call_log_row = call_log.to_data_source(CallLogSchema)
+    session.add(call_log_row)
+    return call_log_row
+
+
+def _link_call_log(session, call_log, call_log_row):
+    for cel_id in call_log.get_related_cels():
+        cel_row = session.query(CELSchema).get(cel_id)
+        call_log_row.cels.append(cel_row)
 
 
 @daosession
