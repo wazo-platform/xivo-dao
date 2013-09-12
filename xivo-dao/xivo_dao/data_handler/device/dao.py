4542fa2717a96736400fdc6f50dac309fe24122f
Cedric Abunar <cabunar@avencall.com>
2013-09-12 12:13:36 -0400

    Revert "Revert "Merge branch 'provd_config'""
    This reverts commit 128d3774f5ceecd7ae9b20d67790d504e9279e6a.

--- xivo-dao/xivo_dao/data_handler/device/dao.py
+++ xivo-dao/xivo_dao/data_handler/device/dao.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+#
 # Copyright (C) 2013 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,102 +14,200 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
-from xivo_dao.alchemy.devicefeatures import DeviceFeatures as DeviceSchema
-from xivo_dao.helpers.db_manager import daosession
+
+
+from urllib2 import HTTPError
+
 from xivo_dao.data_handler.device.model import Device, DeviceOrdering
+from xivo_dao.data_handler.device import provd_builder
 from xivo_dao.data_handler.exception import ElementNotExistsError, \
-    ElementDeletionError, ElementCreationError
-from sqlalchemy.exc import SQLAlchemyError
+    ElementDeletionError, ElementCreationError, InvalidParametersError, \
+    ElementEditionError
 from xivo_dao.helpers import provd_connector
 
 
 DEFAULT_ORDER = [DeviceOrdering.ip, DeviceOrdering.mac]
 
 
-@daosession
-def get(session, device_id):
+def get(device_id):
+    provd_device = _get_provd_device(device_id)
+    return _build_device(provd_device)
+
+
+def _get_provd_device(device_id):
+    device_manager = provd_connector.device_manager()
+
     try:
-        res = (session.query(DeviceSchema).filter(DeviceSchema.id == int(device_id))).first()
-    except ValueError:
-        raise ElementNotExistsError('Device', id=device_id)
+        provd_device = device_manager.get(device_id)
+    except HTTPError as e:
+        if e.code == 404:
+            raise ElementNotExistsError('Device', id=device_id)
+        raise e
 
-    if not res:
-        raise ElementNotExistsError('Device', id=device_id)
-
-    return Device.from_data_source(res)
-
-
-@daosession
-def get_by_deviceid(session, device_id):
-    res = (session.query(DeviceSchema).filter(DeviceSchema.deviceid == device_id)).first()
-
-    if not res:
-        raise ElementNotExistsError('Device', deviceid=device_id)
-
-    return Device.from_data_source(res)
+    return provd_device
 
 
-@daosession
-def find(session, device_id):
-    device_row = session.query(DeviceSchema).filter(DeviceSchema.id == device_id).first()
-
-    if device_row:
-        return Device.from_data_source(device_row)
-    return None
+def _build_device(provd_device):
+    provd_config = _find_provd_config(provd_device)
+    return Device.from_provd(provd_device, provd_config)
 
 
-@daosession
-def find_all(session):
-    rows = (session.query(DeviceSchema).all())
+def _find_provd_config(provd_device):
+    if 'config' not in provd_device:
+        return None
 
-    return [Device.from_data_source(row) for row in rows]
+    config_manager = provd_connector.config_manager()
+
+    provd_config = config_manager.get(provd_device['config'])
+
+    return provd_config
 
 
-@daosession
-def delete(session, device):
-    session.begin()
+def find(device_id):
+    device_manager = provd_connector.device_manager()
+
+    devices = device_manager.find({'id': device_id})
+    if len(devices) == 0:
+        return None
+
+    provd_device = devices[0]
+    return _build_device(provd_device)
+
+
+def find_all(order=None, direction=None, limit=None, skip=None):
+    parameters = _convert_provd_parameters(order, direction, limit, skip)
+
+    device_manager = provd_connector.device_manager()
+    provd_devices = device_manager.find(**parameters)
+
+    return [_build_device(d) for d in provd_devices]
+
+
+def _convert_provd_parameters(order, direction, limit, skip):
+    parameters = {}
+
+    sort = _convert_order_and_direction(order, direction)
+    if sort:
+        parameters['sort'] = sort
+
+    if limit:
+        parameters['limit'] = limit
+
+    if skip:
+        parameters['skip'] = skip
+
+    return parameters
+
+
+def _convert_order_and_direction(order, direction):
+    if direction and not order:
+        raise InvalidParametersError("cannot use a direction without an order")
+
+    if not order:
+        return None
+
+    if direction == 'asc':
+        sort_direction = 1
+    elif direction == 'desc':
+        sort_direction = -1
+    else:
+        sort_direction = 1
+
+    return (order, sort_direction)
+
+
+def delete(device):
+    _delete_provd_device(device)
+    _delete_provd_config(device)
+
+
+def _delete_provd_device(device):
+    provd_device_manager = provd_connector.device_manager()
     try:
-        nb_row_affected = session.query(DeviceSchema).filter(DeviceSchema.id == device.id).delete()
-        session.commit()
-    except SQLAlchemyError, e:
-        session.rollback()
+        provd_device_manager.remove(device.id)
+    except HTTPError as e:
+        if e.code == 404:
+            raise ElementNotExistsError('Device', id=device.id)
+        raise e
+    except Exception as e:
         raise ElementDeletionError('Device', e)
 
-    if nb_row_affected == 0:
-        raise ElementDeletionError('Device', 'device_id %s not exist' % device.id)
 
-    return nb_row_affected
+def _delete_provd_config(device):
+    provd_config_manager = provd_connector.config_manager()
+    try:
+        provd_config_manager.remove(device.id)
+    except Exception as e:
+        pass
 
 
 def create(device):
-    _create_provd_device(device)
-    _create_provd_config(device)
+    device.id = generate_device_id()
+
+    provd_device, provd_config = provd_builder.build_create(device)
+    _create_provd_device(device.id, provd_device)
+    _create_provd_config(device.id, provd_config)
 
     return device
 
 
-def _create_provd_device(device):
+def _create_provd_device(device_id, provd_device):
     device_manager = provd_connector.device_manager()
-    provd_device = device.to_provd_device()
 
     try:
-        device_id = device_manager.add(provd_device)
+        device_manager.update(provd_device)
     except Exception as e:
+        device_manager.remove(device_id)
         raise ElementCreationError('device', e)
 
-    device.id = device_id
 
-
-def _create_provd_config(device):
+def _create_provd_config(device_id, provd_config):
     config_manager = provd_connector.config_manager()
-    provd_config = device.to_provd_config()
 
     try:
         config_manager.add(provd_config)
     except Exception as e:
         device_manager = provd_connector.device_manager()
-        device_manager.remove(device.id)
+        device_manager.remove(device_id)
         raise ElementCreationError('device', e)
+
+
+def generate_device_id():
+    device_manager = provd_connector.device_manager()
+    try:
+        return device_manager.add({})
+    except Exception as e:
+        raise ElementCreationError('device', e)
+
+
+def edit(device):
+    device_manager = provd_connector.device_manager()
+
+    provd_device = device_manager.get(device.id)
+    provd_config = _find_provd_config(provd_device)
+
+    provd_device, provd_config = provd_builder.build_edit(device, provd_device, provd_config)
+
+    if provd_config:
+        _update_provd_config(provd_config)
+
+    _update_provd_device(provd_device)
+
+
+def _update_provd_config(provd_config):
+    config_manager = provd_connector.config_manager()
+    try:
+        config_manager.update(provd_config)
+    except Exception as e:
+        raise ElementEditionError('device', e)
+
+
+def _update_provd_device(provd_device):
+    device_manager = provd_connector.device_manager()
+    try:
+        device_manager.update(provd_device)
+    except Exception as e:
+        raise ElementEditionError('device', e)
 
 
 def mac_exists(mac):
