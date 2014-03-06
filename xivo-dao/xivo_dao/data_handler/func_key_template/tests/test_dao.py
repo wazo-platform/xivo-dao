e8403e8fae9de4626e0867a23e2728f8f444e669
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-03-06 15:58:21 -0500

    refactor template dao tests, move helpers into common unit

--- xivo-dao/xivo_dao/data_handler/func_key_template/tests/test_dao.py
+++ xivo-dao/xivo_dao/data_handler/func_key_template/tests/test_dao.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from hamcrest import assert_that, is_not, none, has_property, equal_to
-from mock import patch, ANY
+from mock import patch, ANY, Mock
 
 from xivo_dao.tests.test_dao import DAOTestCase
 from xivo_dao.alchemy.func_key_template import FuncKeyTemplate as FuncKeyTemplateSchema
@@ -26,6 +26,7 @@ from xivo_dao.alchemy.func_key_type import FuncKeyType as FuncKeyTypeSchema
 from xivo_dao.alchemy.func_key_destination_type import FuncKeyDestinationType as FuncKeyDestinationTypeSchema
 
 from xivo_dao.data_handler.exception import ElementCreationError
+from xivo_dao.data_handler.exception import ElementDeletionError
 from xivo_dao.data_handler.func_key_template import dao
 from xivo_dao.data_handler.func_key.model import FuncKey
 
@@ -34,10 +35,38 @@ class TestFuncKeyTemplateDao(DAOTestCase):
 
     tables = [
         FuncKeyTemplateSchema,
+        FuncKeyMappingSchema,
+        FuncKeySchema,
+        FuncKeyTypeSchema,
+        FuncKeyDestinationTypeSchema,
     ]
 
     def setUp(self):
         self.empty_tables()
+        func_key_type_row = self.add_func_key_type(name='speeddial')
+        destination_type_row = self.add_func_key_destination_type(id=1, name='user')
+
+        self.type_id = func_key_type_row.id
+        self.destination_type_id = destination_type_row.id
+
+    def assert_template_empty(self, template_row):
+        count = (self.session.query(FuncKeyMappingSchema)
+                 .filter(FuncKeyMappingSchema.template_id == template_row.id)
+                 .count())
+
+        assert_that(count, equal_to(0))
+
+    def create_func_key_for_template(self, template_row, position):
+        func_key_row = self.add_func_key(type_id=self.type_id,
+                                         destination_type_id=self.destination_type_id)
+
+        mapping_row = FuncKeyMappingSchema(template_id=template_row.id,
+                                           func_key_id=func_key_row.id,
+                                           destination_type_id=func_key_row.destination_type_id,
+                                           position=position)
+        self.add_me(mapping_row)
+
+        return FuncKey(id=func_key_row.id)
 
 
 class TestCreatePrivateTemplate(TestFuncKeyTemplateDao):
@@ -63,21 +92,6 @@ class TestCreatePrivateTemplate(TestFuncKeyTemplateDao):
 
 class TestRemoveFuncKeyFromTemplate(TestFuncKeyTemplateDao):
 
-    tables = TestFuncKeyTemplateDao.tables + [
-        FuncKeyMappingSchema,
-        FuncKeySchema,
-        FuncKeyTypeSchema,
-        FuncKeyDestinationTypeSchema,
-    ]
-
-    def setUp(self):
-        TestFuncKeyTemplateDao.setUp(self)
-        func_key_type_row = self.add_func_key_type(name='speeddial')
-        destination_type_row = self.add_func_key_destination_type(id=1, name='user')
-
-        self.type_id = func_key_type_row.id
-        self.destination_type_id = destination_type_row.id
-
     def test_given_one_func_key_mapped_when_removed_then_template_empty(self):
         template_row = self.add_func_key_template()
         func_key = self.create_func_key_for_template(template_row, 1)
@@ -95,24 +109,7 @@ class TestRemoveFuncKeyFromTemplate(TestFuncKeyTemplateDao):
 
         self.assert_template_contains_func_key(template_row, second_func_key)
 
-    def create_func_key_for_template(self, template_row, position):
-        func_key_row = self.add_func_key(type_id=self.type_id,
-                                         destination_type_id=self.destination_type_id)
 
-        mapping_row = FuncKeyMappingSchema(template_id=template_row.id,
-                                           func_key_id=func_key_row.id,
-                                           destination_type_id=func_key_row.destination_type_id,
-                                           position=position)
-        self.add_me(mapping_row)
-
-        return FuncKey(id=func_key_row.id)
-
-    def assert_template_empty(self, template_row):
-        count = (self.session.query(FuncKeyMappingSchema)
-                 .filter(FuncKeyMappingSchema.template_id == template_row.id)
-                 .count())
-
-        assert_that(count, equal_to(0))
 
     def assert_template_contains_func_key(self, template_row, func_key_row):
         count = (self.session.query(FuncKeyMappingSchema)
