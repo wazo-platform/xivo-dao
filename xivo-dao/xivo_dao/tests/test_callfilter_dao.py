7a279323aad2abedc23fbd607941add4d5f34fef
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-03-11 13:27:42 -0400

    add unit tests for callfilter_dao.get_secretaries_id_by_context

--- xivo-dao/xivo_dao/tests/test_callfilter_dao.py
+++ xivo-dao/xivo_dao/tests/test_callfilter_dao.py
@@ -19,9 +19,13 @@ from mock import patch, Mock
 from sqlalchemy.exc import SQLAlchemyError
 
 from xivo_dao import callfilter_dao
-from xivo_dao.alchemy.callfilter import Callfilter
 from xivo_dao.tests.test_dao import DAOTestCase
+from xivo_dao.alchemy.callfilter import Callfilter
 from xivo_dao.alchemy.callfiltermember import Callfiltermember
+from xivo_dao.alchemy.user_line import UserLine as UserLineSchema
+from xivo_dao.alchemy.extension import Extension as ExtensionSchema
+from xivo_dao.alchemy.userfeatures import UserFeatures as UserSchema
+from xivo_dao.alchemy.userfeatures import test_dependencies as user_dependencies
 
 
 class BaseTestCallFilterDAO(DAOTestCase):
@@ -49,6 +53,112 @@ class BaseTestCallFilterDAO(DAOTestCase):
         member.bstype = role
         self.add_me(member)
         return member
+
+
+class TestGetSecretariesIdByContext(BaseTestCallFilterDAO):
+
+    tables = BaseTestCallFilterDAO.tables + [
+        UserLineSchema,
+        ExtensionSchema,
+        UserSchema
+    ]
+
+    tables += user_dependencies
+
+    def _create_user_and_add_to_filter(self, filter_id, role, context='mycontext'):
+        user_line_row = self.add_user_line_with_exten(context=context)
+        filter_member_row = self._add_user_to_filter(user_line_row.user_id, filter_id, role)
+        return filter_member_row
+
+    def test_given_no_secretaries_then_returns_empty_list(self):
+        result = callfilter_dao.get_secretaries_id_by_context('default')
+
+        self.assertEquals(result, [])
+
+    def test_given_boss_then_returns_empty_list(self):
+        context = 'mycontext'
+        filter_id = self._insert_call_filter('testfilter')
+        self._create_user_and_add_to_filter(filter_id, 'boss', context)
+
+        result = callfilter_dao.get_secretaries_id_by_context(context)
+
+        self.assertEquals(result, [])
+
+    def test_given_one_secretary_then_returns_list_with_id(self):
+        context = 'mycontext'
+        filter_id = self._insert_call_filter('testfilter')
+        member = self._create_user_and_add_to_filter(filter_id, 'secretary', context)
+
+        result = callfilter_dao.get_secretaries_id_by_context(context)
+
+        self.assertEquals(len(result), 1)
+        self.assertIn((member.id,), result)
+
+    def test_given_two_secretaries_then_returns_list_with_both_ids(self):
+        context = 'mycontext'
+        filter_id = self._insert_call_filter('testfilter')
+        first_member = self._create_user_and_add_to_filter(filter_id, 'secretary', context)
+        second_member = self._create_user_and_add_to_filter(filter_id, 'secretary', context)
+
+        result = callfilter_dao.get_secretaries_id_by_context(context)
+
+        self.assertEquals(len(result), 2)
+        self.assertIn((first_member.id,), result)
+        self.assertIn((second_member.id,), result)
+
+    def test_given_line_with_multiple_users_then_returns_list_with_one_id(self):
+        context = 'mycontext'
+        filter_id = self._insert_call_filter('testfilter')
+        extension = self.add_extension(context=context)
+
+        line = self.add_line(context=context)
+        main_user = self.add_user()
+        secondary_user = self.add_user()
+
+        self.add_user_line(user_id=main_user.id,
+                           line_id=line.id,
+                           extension_id=extension.id,
+                           main_user=True,
+                           main_line=True)
+        self.add_user_line(user_id=secondary_user.id,
+                           line_id=line.id,
+                           extension_id=extension.id,
+                           main_user=False,
+                           main_line=True)
+
+        member = self._add_user_to_filter(main_user.id, filter_id, 'secretary')
+
+        result = callfilter_dao.get_secretaries_id_by_context(context)
+
+        self.assertEquals(len(result), 1)
+        self.assertIn((member.id,), result)
+
+    def test_given_user_with_multiple_line_then_returns_list_with_one_id(self):
+        context = 'mycontext'
+        filter_id = self._insert_call_filter('testfilter')
+        extension = self.add_extension(context=context)
+
+        main_line = self.add_line(context=context)
+        secondary_line = self.add_line(context=context)
+        user = self.add_user()
+
+        self.add_user_line(user_id=user.id,
+                           line_id=main_line.id,
+                           extension_id=extension.id,
+                           main_user=True,
+                           main_line=True)
+        self.add_user_line(user_id=user.id,
+                           line_id=secondary_line.id,
+                           extension_id=extension.id,
+                           main_user=True,
+                           main_line=False)
+
+        member = self._add_user_to_filter(user.id, filter_id, 'secretary')
+
+        result = callfilter_dao.get_secretaries_id_by_context(context)
+
+        self.assertEquals(len(result), 1)
+        self.assertIn((member.id,), result)
 
 
 class TestCallFilterDAO(BaseTestCallFilterDAO):
