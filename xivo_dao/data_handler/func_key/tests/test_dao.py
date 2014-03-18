669f820aaa31b3f800301c95e9e07ef6ed758e9f
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-03-18 15:15:23 -0400

    refactor func key tests. make prerequisites generic

--- xivo_dao/data_handler/func_key/tests/test_dao.py
+++ xivo_dao/data_handler/func_key/tests/test_dao.py
@@ -18,16 +18,15 @@
 from contextlib import contextmanager
 from hamcrest import assert_that, equal_to, instance_of, contains, is_not, \
     none, has_property, contains_inanyorder
-from mock import patch, Mock
+from mock import patch
 
 from xivo_dao.tests.test_dao import DAOTestCase
 
 from xivo_dao.data_handler.exception import ElementNotExistsError
 from xivo_dao.data_handler.exception import ElementCreationError
 from xivo_dao.data_handler.exception import ElementDeletionError
-from xivo_dao.data_handler.func_key.model import FuncKey, QueryHelper
+from xivo_dao.data_handler.func_key.model import FuncKey
 from xivo_dao.data_handler.func_key import dao
-from xivo_dao.helpers.abstract_model import SearchResult
 from xivo_dao.alchemy.func_key import FuncKey as FuncKeySchema
 from xivo_dao.alchemy.func_key_type import FuncKeyType as FuncKeyTypeSchema
 from xivo_dao.alchemy.func_key_dest_user import FuncKeyDestUser as FuncKeyDestUserSchema
@@ -56,70 +55,61 @@ class BaseTestFuncKeyDao(DAOTestCase):
 
 class TestFuncKeyDao(BaseTestFuncKeyDao):
 
+    destination_type_ids = {
+        'user': 1,
+        'group': 2,
+    }
+
+    destination_type_schemas = {
+        'user': (FuncKeyDestUserSchema, 'user_id'),
+        'group': (FuncKeyDestGroupSchema, 'group_id'),
+    }
+
     def setUp(self):
         BaseTestFuncKeyDao.setUp(self)
         self.create_types_and_destinations()
 
     def create_types_and_destinations(self):
-        func_key_type_row = self.add_func_key_type(name='speeddial')
-        user_destination_row = self.add_func_key_destination_type(id=1, name='user')
-        group_destination_row = self.add_func_key_destination_type(id=2, name='group')
+        type_row = self.add_func_key_type(name='speeddial')
+        self.type_id = type_row.id
 
-        self.type_id = func_key_type_row.id
-        self.user_destination_id = user_destination_row.id
-        self.group_destination_id = group_destination_row.id
+        for destination_name, destination_id in self.destination_type_ids.items():
+            self.add_func_key_destination_type(id=destination_id, name=destination_name)
 
-    def add_func_key_for_user(self, user_row):
+    def add_destination(self, destination, destination_id):
+        destination_type_id = self.destination_type_ids[destination]
+        schema, column = self.destination_type_schemas[destination]
+
         func_key_row = self.add_func_key(type_id=self.type_id,
-                                         destination_type_id=self.user_destination_id)
+                                         destination_type_id=destination_type_id)
 
-        dest_user = FuncKeyDestUserSchema(user_id=user_row.id,
-                                          func_key_id=func_key_row.id,
-                                          destination_type_id=self.user_destination_id)
-        self.add_me(dest_user)
+        destination_row = schema(**{'func_key_id': func_key_row.id,
+                                    column: destination_id})
+        self.add_me(destination_row)
 
-        return func_key_row
+        return func_key_row, destination_row
 
-    def add_func_key_for_group(self, group_row):
-        func_key_row = self.add_func_key(type_id=self.type_id,
-                                         destination_type_id=self.group_destination_id)
-
-        dest_group = FuncKeyDestGroupSchema(group_id=group_row.id,
-                                            func_key_id=func_key_row.id,
-                                            destination_type_id=self.group_destination_id)
-        self.add_me(dest_group)
-
-        return func_key_row
-
-    def prepare_user_destination(self, user_row):
-        func_key_row = self.add_func_key_for_user(user_row)
+    def prepare_destination(self, destination, destination_id):
+        func_key_row, destination_row = self.add_destination(destination, destination_id)
 
         return FuncKey(id=func_key_row.id,
                        type='speeddial',
-                       destination='user',
-                       destination_id=user_row.id)
+                       destination=destination,
+                       destination_id=destination_id)
 
-    def prepare_group_destination(self, group_row):
-        func_key_row = self.add_func_key_for_group(group_row)
+    def find_destination(self, destination, destination_id):
+        schema, column_name = self.destination_type_schemas[destination]
+        column = getattr(schema, column_name)
 
-        return FuncKey(id=func_key_row.id,
-                       type='speeddial',
-                       destination='group',
-                       destination_id=group_row.id)
-
-    def find_user_destination(self, user_id):
-        row = (self.session.query(FuncKeyDestUserSchema)
-               .filter(FuncKeyDestUserSchema.user_id == user_id)
+        row = (self.session.query(schema)
+               .filter(column == destination_id)
                .first())
 
         return row
 
-    def find_group_destination(self, group_id):
-        row = (self.session.query(FuncKeyDestGroupSchema)
-               .filter(FuncKeyDestGroupSchema.group_id == group_id)
-               .first())
-
-        return row
+    def assert_destination_deleted(self, destination, destination_id):
+        row = self.find_destination(destination, destination_id)
+        assert_that(row, none())
 
     @contextmanager
     def mocked_session(self):
@@ -139,7 +129,7 @@ class TestFuncKeySearch(TestFuncKeyDao):
 
     def test_given_user_destination_when_searching_then_one_result_returned(self):
         user_row = self.add_user()
-        func_key = self.prepare_user_destination(user_row)
+        func_key = self.prepare_destination('user', user_row.id)
 
         result = dao.search()
 
@@ -149,8 +139,8 @@ class TestFuncKeySearch(TestFuncKeyDao):
     def test_given_user_and_group_destination_when_searching_then_two_results_returned(self):
         user_row = self.add_user()
         group_row = self.add_group()
-        user_destination = self.prepare_user_destination(user_row)
-        group_destination = self.prepare_group_destination(group_row)
+        user_destination = self.prepare_destination('user', user_row.id)
+        group_destination = self.prepare_destination('group', group_row.id)
 
         result = dao.search()
         assert_that(result.total, equal_to(2))
@@ -158,7 +148,7 @@ class TestFuncKeySearch(TestFuncKeyDao):
 
     def test_given_func_key_without_destination_when_searching_then_returns_nothing(self):
         self.add_func_key(type_id=self.type_id,
-                          destination_type_id=self.user_destination_id)
+                          destination_type_id=self.destination_type_ids['user'])
 
         result = dao.search()
         assert_that(result.total, equal_to(0))
@@ -174,7 +164,7 @@ class TestFuncKeyFindAllByDestination(TestFuncKeyDao):
 
     def test_given_one_user_destination_then_returns_list_with_one_element(self):
         user_row = self.add_user()
-        func_key = self.prepare_user_destination(user_row)
+        func_key = self.prepare_destination('user', user_row.id)
 
         result = dao.find_all_by_destination('user', user_row.id)
 
@@ -184,8 +174,8 @@ class TestFuncKeyFindAllByDestination(TestFuncKeyDao):
         first_user = self.add_user()
         second_user = self.add_user()
 
-        self.prepare_user_destination(first_user)
-        func_key = self.prepare_user_destination(second_user)
+        self.prepare_destination('user', first_user.id)
+        func_key = self.prepare_destination('user', second_user.id)
 
         result = dao.find_all_by_destination('user', second_user.id)
 
@@ -193,23 +183,23 @@ class TestFuncKeyFindAllByDestination(TestFuncKeyDao):
 
     def test_given_one_group_destination_then_returns_list_with_one_group_destination(self):
         group_row = self.add_group()
-        func_key = self.prepare_group_destination(group_row)
+        func_key = self.prepare_destination('group', group_row.id)
 
         result = dao.find_all_by_destination('group', group_row.id)
         assert_that(result, contains(func_key))
 
     def test_given_group_and_user_destination_then_returns_list_with_right_destination(self):
         user_row = self.add_user()
-        self.prepare_user_destination(user_row)
+        self.prepare_destination('user', user_row.id)
         group_row = self.add_group()
-        group_func_key = self.prepare_group_destination(group_row)
+        group_func_key = self.prepare_destination('group', group_row.id)
 
         result = dao.find_all_by_destination('group', group_row.id)
         assert_that(result, contains(group_func_key))
 
     def test_given_user_destination_when_searching_wrong_type_then_returns_empty_list(self):
         user_row = self.add_user()
-        self.prepare_user_destination(user_row)
+        self.prepare_destination('user', user_row.id)
 
         result = dao.find_all_by_destination('invalidtype', user_row.id)
 
@@ -223,7 +213,7 @@ class TestFuncKeyGet(TestFuncKeyDao):
 
     def test_when_user_func_key_in_db_then_func_key_model_returned(self):
         user_row = self.add_user()
-        func_key = self.prepare_user_destination(user_row)
+        func_key = self.prepare_destination('user', user_row.id)
 
         result = dao.get(func_key.id)
 
@@ -231,7 +221,7 @@ class TestFuncKeyGet(TestFuncKeyDao):
 
     def test_when_group_func_key_in_db_then_func_key_model_returned(self):
         group_row = self.add_group()
-        func_key = self.prepare_group_destination(group_row)
+        func_key = self.prepare_destination('group', group_row.id)
 
         result = dao.get(func_key.id)
 
@@ -240,8 +230,8 @@ class TestFuncKeyGet(TestFuncKeyDao):
     def test_when_two_func_keys_in_db_then_right_model_returned(self):
         user_row = self.add_user()
 
-        self.prepare_user_destination(user_row)
-        second_func_key = self.prepare_user_destination(user_row)
+        self.prepare_destination('user', user_row.id)
+        second_func_key = self.prepare_destination('user', user_row.id)
 
         result = dao.get(second_func_key.id)
 
@@ -261,7 +251,7 @@ class TestFuncKeyCreate(TestFuncKeyDao):
         assert_that(created_func_key, instance_of(FuncKey))
         assert_that(created_func_key, has_property('id', is_not(none())))
 
-        user_destination_row = self.find_user_destination(user_row.id)
+        user_destination_row = self.find_destination('user', user_row.id)
         assert_that(user_destination_row, is_not(none()))
 
         self.assert_func_key_row_created(user_destination_row)
@@ -277,7 +267,7 @@ class TestFuncKeyCreate(TestFuncKeyDao):
         assert_that(created_func_key, instance_of(FuncKey))
         assert_that(created_func_key, has_property('id', is_not(none())))
 
-        group_destination_row = self.find_group_destination(group_row.id)
+        group_destination_row = self.find_destination('group', group_row.id)
         assert_that(group_destination_row, is_not(none()))
 
         self.assert_func_key_row_created(group_destination_row)
@@ -303,21 +293,21 @@ class TestFuncKeyDelete(TestFuncKeyDao):
 
     def test_given_user_destination_then_func_key_deleted(self):
         user_row = self.add_user()
-        func_key = self.prepare_user_destination(user_row)
+        func_key = self.prepare_destination('user', user_row.id)
 
         dao.delete(func_key)
 
         self.assert_func_key_deleted(func_key.id)
-        self.assert_user_destination_deleted(user_row.id)
+        self.assert_destination_deleted('user', user_row.id)
 
     def test_given_group_destination_then_func_key_deleted(self):
         group_row = self.add_group()
-        func_key = self.prepare_group_destination(group_row)
+        func_key = self.prepare_destination('group', group_row.id)
 
         dao.delete(func_key)
 
         self.assert_func_key_deleted(func_key.id)
-        self.assert_group_destination_deleted(group_row.id)
+        self.assert_destination_deleted('group', group_row.id)
 
     @patch('xivo_dao.data_handler.func_key.dao.commit_or_abort')
     def test_given_db_error_then_transaction_rollbacked(self, commit_or_abort):
@@ -333,12 +323,4 @@ class TestFuncKeyDelete(TestFuncKeyDao):
         row = (self.session.query(FuncKeySchema)
                .filter(FuncKeySchema.id == func_key_id)
                .first())
-        assert_that(row, none())
-
-    def assert_user_destination_deleted(self, user_id):
-        row = self.find_user_destination(user_id)
-        assert_that(row, none())
-
-    def assert_group_destination_deleted(self, group_id):
-        row = self.find_group_destination(group_id)
         assert_that(row, none())
