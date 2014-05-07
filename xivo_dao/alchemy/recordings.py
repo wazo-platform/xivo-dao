0de859118c58d121f0da444c08eba1ff389a4bb6
Cedric Abunar <cabunar@avencall.com>
2014-05-07 14:44:11 -0400

    update model to reflect database

--- xivo_dao/alchemy/recordings.py
+++ xivo_dao/alchemy/recordings.py
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.schema import Column, PrimaryKeyConstraint, \
+    ForeignKeyConstraint
 from sqlalchemy.types import String, Integer, DateTime
 from sqlalchemy.orm import relationship
 
@@ -25,16 +26,25 @@ from xivo_dao.helpers.db_manager import Base
 class Recordings(Base):
 
     __tablename__ = 'recording'
+    __table_args__ = (
+        PrimaryKeyConstraint('id'),
+        ForeignKeyConstraint(('campaign_id',),
+                             ('record_campaign.id',)),
+        ForeignKeyConstraint(('agent_id',),
+                             ('agentfeatures.id',),
+                             ondelete='SET NULL',
+                             onupdate='CASCADE'),
+    )
 
-    cid = Column(String(32), primary_key=True)
+    id = Column(String(32))
     start_time = Column(DateTime)
     end_time = Column(DateTime)
     caller = Column(String(32))
     client_id = Column(String(1024))
     callee = Column(String(32))
     filename = Column(String(1024))
-    campaign_id = Column(Integer, ForeignKey('record_campaign.id'), nullable=False)
-    agent_id = Column(Integer, ForeignKey('agentfeatures.id'), nullable=False)
+    campaign_id = Column(Integer, nullable=False)
+    agent_id = Column(Integer, nullable=False)
 
-    record_campaign = relationship("RecordCampaigns", foreign_keys=campaign_id)
-    agentfeatures = relationship("AgentFeatures", foreign_keys=agent_id)
+    record_campaign = relationship("RecordCampaigns")
+    agentfeatures = relationship("AgentFeatures")
