0437ac0b1fed46c88f9291fce1800df8ce5dc530
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-06-04 11:45:53 -0400

    add a name to the entity id foreignkey in callfilter

--- xivo_dao/alchemy/callfilter.py
+++ xivo_dao/alchemy/callfilter.py
@@ -31,6 +31,7 @@ class Callfilter(Base):
         PrimaryKeyConstraint('id'),
         ForeignKeyConstraint(('entity_id',),
                              ('entity.id',),
+                             name='fk_entity_id',
                              ondelete='RESTRICT'),
         UniqueConstraint('name'),
     )
