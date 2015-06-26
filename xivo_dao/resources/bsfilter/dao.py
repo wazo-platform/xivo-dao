0bbe93b6fb8e68fbdf7c8a64c9d7fb727bc0d4b0
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-06-26 15:41:15 -0400

    add getter for bsfilter members
    used by confd for validation

--- xivo_dao/resources/bsfilter/dao.py
+++ xivo_dao/resources/bsfilter/dao.py
@@ -16,8 +16,11 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from sqlalchemy import sql, Integer
+
 from xivo_dao.alchemy.callfiltermember import Callfiltermember
 from xivo_dao.helpers.db_manager import daosession
+from xivo_dao.resources.bsfilter.model import FilterMember
 
 
 @daosession
@@ -27,3 +30,17 @@ def filter_member_exists(session, filter_member_id):
              )
 
     return query.count() > 0
+
+
+@daosession
+def find_all_by_member_id(session, member_id):
+    query = (session.query(Callfiltermember.id,
+                           sql.cast(Callfiltermember.typeval, Integer).label('member_id'),
+                           Callfiltermember.bstype.label('role'))
+             .filter(Callfiltermember.type == 'user')
+             .filter(Callfiltermember.bstype.in_(['boss', 'secretary']))
+             .filter(sql.cast(Callfiltermember.typeval, Integer) == member_id)
+             )
+
+    return [FilterMember(id=row.id, member_id=row.member_id, role=row.role)
+            for row in query]
--- xivo_dao/resources/bsfilter/tests/test_dao.py
+++ xivo_dao/resources/bsfilter/tests/test_dao.py
@@ -16,10 +16,12 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, has_items, contains
 
 from xivo_dao.tests.test_dao import DAOTestCase
 from xivo_dao.resources.bsfilter import dao as bsfilter_dao
+
+from xivo_dao.resources.bsfilter.model import FilterMember
 
 
 class TestFilterMemberExist(DAOTestCase):
@@ -36,3 +38,43 @@ class TestFilterMemberExist(DAOTestCase):
         result = bsfilter_dao.filter_member_exists(filter_member_row.id)
 
         assert_that(result, equal_to(True))
+
+
+class TestFindByMemberId(DAOTestCase):
+
+    def test_given_no_callfilter_or_members_then_returns_empty_list(self):
+        result = bsfilter_dao.find_all_by_member_id(1)
+
+        assert_that(result, contains())
+
+    def test_when_filter_has_boss_and_secretary_then_returns_memberships(self):
+        boss_id = 10
+        secretary_id = 20
+
+        callfilter_row = self.add_bsfilter()
+        boss_member_row = self.add_filter_member(callfilter_row.id, boss_id, 'boss')
+        secretary_member_row = self.add_filter_member(callfilter_row.id, secretary_id, 'secretary')
+
+        boss_member = FilterMember(id=boss_member_row.id, member_id=boss_id, role='boss')
+        secretary_member = FilterMember(id=secretary_member_row.id, member_id=secretary_id, role='secretary')
+
+        result = bsfilter_dao.find_all_by_member_id(boss_id)
+        assert_that(result, contains(boss_member))
+
+        result = bsfilter_dao.find_all_by_member_id(secretary_id)
+        assert_that(result, contains(secretary_member))
+
+    def test_when_member_is_in_two_filters_then_returns_both_memberships(self):
+        boss_id = 10
+
+        first_callfilter = self.add_bsfilter(name='first')
+        first_membership_row = self.add_filter_member(first_callfilter.id, boss_id, 'boss')
+
+        second_callfilter = self.add_bsfilter(name='second')
+        second_membership_row = self.add_filter_member(second_callfilter.id, boss_id, 'boss')
+
+        first_member = FilterMember(id=first_membership_row.id, member_id=boss_id, role='boss')
+        second_member = FilterMember(id=second_membership_row.id, member_id=boss_id, role='boss')
+
+        result = bsfilter_dao.find_all_by_member_id(boss_id)
+        assert_that(result, has_items(first_member, second_member))
