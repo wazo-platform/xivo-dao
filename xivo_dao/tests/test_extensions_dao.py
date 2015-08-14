94040cb9ed56c81102473a5045ea63efe931ebb7
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-08-14 14:54:47 -0400

    extensions_dao remove unused code
    remove create
    remove get_by_exten
    remove delete_by_exten

--- xivo_dao/tests/test_extensions_dao.py
+++ xivo_dao/tests/test_extensions_dao.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2007-2014 Avencall
+# Copyright (C) 2007-2015 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from xivo_dao import extensions_dao
-from xivo_dao.alchemy.extension import Extension
 from xivo_dao.tests.test_dao import DAOTestCase
 
 
@@ -36,35 +35,3 @@ class TestExtensionsDAO(DAOTestCase):
         self.assertEqual(enablednd, '*25')
         self.assertEqual(phoneprogfunckey, '_*735')
         self.assertEqual(foo, '')
-
-    def test_create(self):
-        exten = Extension()
-        exten.type = 'user'
-        exten.exten = '2000'
-        exten.context = 'default'
-
-        extensions_dao.create(exten)
-        self.assertTrue(exten.id)
-        self.assertTrue(exten in self._get_all())
-        self.assertEquals(exten.name, exten.typeval)
-
-    def test_get_by_exten(self):
-        self.add_extension(type='extenfeatures',
-                           typeval='enablednd',
-                           exten='*25')
-        result = extensions_dao.get_by_exten('*25')
-        self.assertEquals('*25', result.exten)
-
-    def _get_all(self):
-        return self.session.query(Extension).all()
-
-    def test_delete_by_exten(self):
-        self.add_extension(type='extenfeatures',
-                           typeval='enablednd',
-                           exten='*25')
-        self.add_extension(type='extenfeatures',
-                           typeval='phoneprogfunckey',
-                           exten='_*735')
-        extensions_dao.delete_by_exten("*25")
-        self.assertFalse("*25" in [item.exten for item in self._get_all()])
-        self.assertTrue("_*735" in [item.exten for item in self._get_all()])
