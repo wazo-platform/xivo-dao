6ca2ba57eab014d59b48250526e38026d9f05b0b
Sebastien Duthil <sduthil@avencall.com>
2015-11-26 09:27:49 -0500

    restore user_dao.py with one function
    reason: this function is used only in xivo-agid, and tests should
    follow it if extracted

---
+++ xivo_dao/tests/test_user_dao.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012-2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from hamcrest import assert_that
+from hamcrest import equal_to
+
+from xivo_dao import user_dao
+from xivo_dao.tests.test_dao import DAOTestCase
+
+
+class TestUserFeaturesDAO(DAOTestCase):
+
+    def test_get_user_by_number_context(self):
+        context, number = 'default', '1234'
+        user_line = self.add_user_line_with_exten(exten=number,
+                                                  context=context)
+
+        user = user_dao.get_user_by_number_context(number, context)
+
+        assert_that(user.id, equal_to(user_line.user.id))
+
+    def test_get_user_by_number_context_line_commented(self):
+        context, number = 'default', '1234'
+        self.add_user_line_with_exten(exten=number,
+                                      context=context,
+                                      commented_line=1)
+
+        self.assertRaises(LookupError, user_dao.get_user_by_number_context, number, context)
---
+++ xivo_dao/user_dao.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012-2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import logging
+
+from sqlalchemy import and_
+
+from xivo_dao.alchemy.linefeatures import LineFeatures
+from xivo_dao.alchemy.userfeatures import UserFeatures
+from xivo_dao.helpers.db_manager import daosession
+from xivo_dao.alchemy.user_line import UserLine
+from xivo_dao.alchemy.extension import Extension as ExtensionSchema
+
+logger = logging.getLogger(__name__)
+
+
+@daosession
+def get_user_by_number_context(session, exten, context):
+    user = (session.query(UserFeatures)
+            .join(ExtensionSchema, and_(ExtensionSchema.context == context,
+                                        ExtensionSchema.exten == exten,
+                                        ExtensionSchema.commented == 0))
+            .join(LineFeatures, and_(LineFeatures.commented == 0))
+            .join(UserLine, and_(UserLine.user_id == UserFeatures.id,
+                                 UserLine.extension_id == ExtensionSchema.id,
+                                 UserLine.line_id == LineFeatures.id,
+                                 UserLine.main_line == True,
+                                 UserLine.main_line == True))
+            .first())
+
+    if not user:
+        raise LookupError('No user with number %s in context %s', (exten, context))
+
+    return user
