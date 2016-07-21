dd132caccd23950f260f6e8b4a372c31133ee7a5
Francois Blackburn <fblackburn@proformatique.com>
2016-07-21 15:02:42 -0400

    user_line: fix main_line_rel
    The main line of a user are not necessary to main user of the line

--- xivo_dao/alchemy/user_line.py
+++ xivo_dao/alchemy/user_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013-2014 Avencall
+# Copyright (C) 2013-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -54,6 +54,5 @@ class UserLine(Base):
 
     main_line_rel = relationship("LineFeatures",
                                  primaryjoin="""and_(UserLine.line_id == LineFeatures.id,
-                                 UserLine.main_user == True,
                                  UserLine.main_line == True)"""
                                  )
