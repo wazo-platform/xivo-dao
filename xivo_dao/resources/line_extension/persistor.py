a10798329e7b35007d01b07f74695c22a1d9d831
Francois Blackburn <fblackburn@proformatique.com>
2016-07-22 14:30:40 -0400

    add persistor line_extension

---
+++ xivo_dao/resources/line_extension/persistor.py
@@ -0,0 +1,82 @@
+# -*- coding: UTF-8 -*-
+
+# Copyright (C) 2015-2016 Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
+from xivo_dao.resources.extension.fixes import ExtensionFixes
+from xivo_dao.resources.line.fixes import LineFixes
+
+from xivo_dao.helpers import errors
+from xivo_dao.alchemy.line_extension import LineExtension
+
+
+class LineExtensionPersistor(CriteriaBuilderMixin):
+
+    _search_table = LineExtension
+
+    def __init__(self, session):
+        self.session = session
+
+    def find_query(self, **criteria):
+        query = self.session.query(LineExtension)
+        return self.build_criteria(query, criteria)
+
+    def find_by(self, **criteria):
+        return self.find_query(**criteria).first()
+
+    def get_by(self, **criteria):
+        line_extension = self.find_by(**criteria)
+        if not line_extension:
+            raise errors.not_found('LineExtension', **criteria)
+        return line_extension
+
+    def find_all_by(self, **criteria):
+        return self.find_query(**criteria).all()
+
+    def associate_line_extension(self, line, extension):
+        line_main_extension = self.find_by(main_extension=True, line_id=line.id)
+
+        line_extension = LineExtension(line_id=line.id,
+                                       extension_id=extension.id,
+                                       main_extension=(False if line_main_extension else True))
+
+        self.session.add(line_extension)
+        self.session.flush()
+        ExtensionFixes(self.session).fix_extension(line_extension.extension_id)
+        LineFixes(self.session).fix(line_extension.line_id)
+
+        return [line_extension]
+
+    def dissociate_line_extension(self, line, extension):
+        line_extension = self.get_by(line_id=line.id, extension_id=extension.id)
+
+        if line_extension.main_extension:
+            self._set_random_main_extension(line)
+
+        self.session.delete(line_extension)
+        self.session.flush()
+        ExtensionFixes(self.session).fix_extension(line_extension.extension_id)
+        LineFixes(self.session).fix(line_extension.line_id)
+
+        return line_extension
+
+    def _set_random_main_extension(self, line):
+        random_line_extension = self.find_by(line_id=line.id,
+                                             main_extension=False)
+        if random_line_extension:
+            random_line_extension.main_extension = True
+            self.session.add(random_line_extension)
