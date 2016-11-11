e182800381addcecd63bde31bf5636851220b50b
Francois Blackburn <fblackburn@proformatique.com>
2016-11-11 06:36:13 -0500

    outcall: move tests from dao to alchemy
    reason: We tests properties (or methods) of Outcall model, so it's not
    dao tests. This also help to decrease the dao with the aim of (maybe)
    removing it in the future.

--- xivo_dao/resources/outcall/tests/test_dao.py
+++ xivo_dao/resources/outcall/tests/test_dao.py
@@ -17,22 +17,16 @@
 
 from hamcrest import (assert_that,
                       contains,
-                      contains_inanyorder,
                       equal_to,
-                      empty,
                       has_items,
                       has_properties,
                       has_property,
                       is_not,
-                      none,
-                      not_)
+                      none)
 
-from xivo_dao.alchemy.dialpattern import DialPattern
 from xivo_dao.alchemy.extension import Extension
 from xivo_dao.alchemy.outcall import Outcall
-from xivo_dao.alchemy.outcalltrunk import OutcallTrunk
 from xivo_dao.alchemy.rightcallmember import RightCallMember
-from xivo_dao.alchemy.trunkfeatures import TrunkFeatures
 from xivo_dao.alchemy.rightcall import RightCall
 from xivo_dao.alchemy.schedule import Schedule
 from xivo_dao.alchemy.schedulepath import SchedulePath
@@ -349,9 +343,6 @@ class TestDelete(DAOTestCase):
 
         outcall_dao.delete(outcall)
 
-        dialpattern = self.session.query(DialPattern).first()
-        assert_that(dialpattern, none())
-
         row = self.session.query(Extension).first()
         assert_that(row.id, equal_to(extension.id))
         assert_that(row, has_properties(type='user', typeval='0'))
@@ -381,141 +372,3 @@ class TestDelete(DAOTestCase):
 
         row = self.session.query(Schedule).first()
         assert_that(row.id, equal_to(schedule.id))
-
-
-class TestAssociateTrunk(DAOTestCase):
-
-    def test_outcalls_create(self):
-        trunk = TrunkFeatures()
-        outcall_row = self.add_outcall()
-        outcall_row.trunks = [trunk]
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.trunks, contains_inanyorder(trunk))
-
-    def test_outcalls_association(self):
-        outcall_row = self.add_outcall()
-        trunk1_row = self.add_trunk()
-        trunk2_row = self.add_trunk()
-        trunk3_row = self.add_trunk()
-        outcall_row.trunks = [trunk2_row, trunk3_row, trunk1_row]
-
-        outcall = outcall_dao.get(outcall_row.id)
-        assert_that(outcall.trunks, contains(trunk2_row, trunk3_row, trunk1_row))
-
-    def test_outcalls_dissociation(self):
-        outcall_row = self.add_outcall()
-        trunk1_row = self.add_trunk()
-        trunk2_row = self.add_trunk()
-        trunk3_row = self.add_trunk()
-        outcall_row.trunks = [trunk2_row, trunk3_row, trunk1_row]
-        outcall = outcall_dao.get(outcall_row.id)
-        assert_that(outcall.trunks, not_(empty()))
-        outcall_row.trunks = []
-
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.trunks, empty())
-
-        row = self.session.query(TrunkFeatures).first()
-        assert_that(row, not_(none()))
-
-        row = self.session.query(OutcallTrunk).first()
-        assert_that(row, none())
-
-
-class TestAssociateExtension(DAOTestCase):
-
-    def test_associate_extension(self):
-        extension = self.add_extension()
-        outcall_row = self.add_outcall()
-        outcall_row.associate_extension(extension,
-                                        strip_digits=5,
-                                        caller_id='toto',
-                                        external_prefix='123',
-                                        prefix='321')
-
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.context, equal_to(extension.context))
-        assert_that(outcall.dialpatterns, contains_inanyorder(
-            has_properties(strip_digits=5,
-                           caller_id='toto',
-                           external_prefix='123',
-                           prefix='321',
-                           type='outcall',
-                           typeid=outcall.id,
-                           exten=extension.exten,
-                           extension=has_properties(exten=extension.exten,
-                                                    context=extension.context,
-                                                    type='outcall',
-                                                    typeval=outcall.dialpatterns[0].id))
-        ))
-
-    def test_associate_multiple_extensions(self):
-        outcall_row = self.add_outcall()
-        extension1_row = self.add_extension()
-        extension2_row = self.add_extension()
-        extension3_row = self.add_extension()
-        outcall_row.associate_extension(extension1_row, caller_id='toto')
-        outcall_row.associate_extension(extension2_row, strip_digits=5)
-        outcall_row.associate_extension(extension3_row, external_prefix='123', prefix='321')
-
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.extensions, contains_inanyorder(extension1_row, extension2_row, extension3_row))
-
-    def test_extensions_dissociation(self):
-        outcall_row = self.add_outcall()
-        extension1_row = self.add_extension()
-        extension2_row = self.add_extension()
-        extension3_row = self.add_extension()
-        outcall_row.associate_extension(extension1_row)
-        outcall_row.associate_extension(extension2_row)
-        outcall_row.associate_extension(extension3_row)
-
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.extensions, not_(empty()))
-
-        outcall_row.dissociate_extension(extension1_row)
-        outcall_row.dissociate_extension(extension2_row)
-        outcall_row.dissociate_extension(extension3_row)
-        outcall = outcall_dao.get(outcall_row.id)
-
-        assert_that(outcall, equal_to(outcall_row))
-        assert_that(outcall.extensions, empty())
-        assert_that(outcall.context, none())
-
-        rows = self.session.query(Extension).all()
-        assert_that(rows, contains_inanyorder(has_properties(type='user', typeval='0'),
-                                              has_properties(type='user', typeval='0'),
-                                              has_properties(type='user', typeval='0')))
-
-        row = self.session.query(DialPattern).first()
-        assert_that(row, none())
-
-    def test_update_extension_association(self):
-        outcall_row = self.add_outcall()
-        extension_row = self.add_extension()
-        outcall_row.associate_extension(extension_row, caller_id='toto', prefix='1')
-
-        outcall_row.update_extension_association(extension_row,
-                                                 caller_id='tata',
-                                                 external_prefix='123')
-
-        rows = self.session.query(Extension).all()
-        assert_that(rows, contains(extension_row))
-
-        rows = self.session.query(DialPattern).all()
-        assert_that(rows, contains(has_properties(
-            caller_id='tata',
-            external_prefix='123',
-            prefix='1',
-            strip_digits=0
-        )))
