4a8e247f06e8f9a6c459da977c537871e34533c0
Sebastien Duthil <sduthil@proformatique.com>
2016-11-23 13:17:20 -0500

    add switchboard table + dao

---
+++ xivo_dao/alchemy/switchboard.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import uuid
+
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import Column, PrimaryKeyConstraint
+from sqlalchemy.types import String
+
+from xivo_dao.helpers.db_manager import Base
+
+
+def _new_uuid():
+    return str(uuid.uuid4())
+
+
+class Switchboard(Base):
+
+    __tablename__ = 'switchboard'
+    __table_args__ = (
+        PrimaryKeyConstraint('id'),
+    )
+
+    id = Column(String(38), nullable=False, default=_new_uuid)
+    name = Column(String(128), nullable=False)
---
+++ xivo_dao/resources/switchboard/dao.py
@@ -0,0 +1,67 @@
+# -*- coding: UTF-8 -*-
+
+# Copyright (C) 2016 Proformatique Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from .persistor import SwitchboardPersistor
+from .search import switchboard_search
+
+from xivo_dao.helpers.db_manager import daosession
+
+
+@daosession
+def search(session, **parameters):
+    return SwitchboardPersistor(session, switchboard_search).search(parameters)
+
+
+@daosession
+def get(session, switchboard_id):
+    return SwitchboardPersistor(session, switchboard_search).get_by({'id': switchboard_id})
+
+
+@daosession
+def get_by(session, **criteria):
+    return SwitchboardPersistor(session, switchboard_search).get_by(criteria)
+
+
+@daosession
+def find(session, switchboard_id):
+    return SwitchboardPersistor(session, switchboard_search).find_by({'id': switchboard_id})
+
+
+@daosession
+def find_by(session, **criteria):
+    return SwitchboardPersistor(session, switchboard_search).find_by(criteria)
+
+
+@daosession
+def find_all_by(session, **criteria):
+    return SwitchboardPersistor(session, switchboard_search).find_all_by(criteria)
+
+
+@daosession
+def create(session, switchboard):
+    return SwitchboardPersistor(session, switchboard_search).create(switchboard)
+
+
+@daosession
+def edit(session, switchboard):
+    return SwitchboardPersistor(session, switchboard_search).edit(switchboard)
+
+
+@daosession
+def delete(session, switchboard):
+    SwitchboardPersistor(session, switchboard_search).delete(switchboard)
