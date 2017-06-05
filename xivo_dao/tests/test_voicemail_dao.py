72f9065f10c9c9f27c4991440d5b2f3bed8598f5
Pascal Cadotte Michaud <pcm@wazo.community>
2017-06-05 14:16:47 -0400

    copyright: update copyright notice for all files in this branch

--- xivo_dao/tests/test_voicemail_dao.py
+++ xivo_dao/tests/test_voicemail_dao.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2017 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
