7690f4f82800d6d58d626b72e5beabb439ec69d1
Pascal Cadotte Michaud <pcm@wazo.community>
2017-07-10 07:54:23 -0400

    update copyright notices for modified files

--- xivo_dao/resources/user_line/persistor.py
+++ xivo_dao/resources/user_line/persistor.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (C) 2015-2017 Avencall
+# Copyright 2015-2017 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
