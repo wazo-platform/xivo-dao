113807ee09af1b284891b1fc3364f8f7b2b6e1c1
Francois Blackburn <fblackburn@wazo.community>
2017-11-22 14:58:59 -0500

    add register_sip resource

---
+++ xivo_dao/resources/register_sip/search.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_dao.alchemy.staticsip import StaticSIP as RegisterSIP
+from xivo_dao.resources.utils.search import SearchSystem
+from xivo_dao.resources.utils.search import SearchConfig
+
+
+config = SearchConfig(table=RegisterSIP,
+                      columns={'id': RegisterSIP.id},
+                      default_sort='id')
+
+
+class RegisterSIPSearchSystem(SearchSystem):
+
+    def search(self, session, parameters=None):
+        query = session.query(self.config.table).filter(RegisterSIP.var_name == 'register')
+        return self.search_from_query(query, parameters)
+
+
+register_sip_search = RegisterSIPSearchSystem(config)
