cc1bd6abc3b6f4c46e03851b3afd8e73ce487818
Francois Blackburn <fblackburn@wazo.community>
2017-12-05 11:05:36 -0500

    add iax_general resource

--- xivo_dao/alchemy/staticiax.py
+++ xivo_dao/alchemy/staticiax.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from sqlalchemy.schema import Column, PrimaryKeyConstraint, Index
 from sqlalchemy.types import Integer, String
+from sqlalchemy.sql import func
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from xivo_dao.helpers.db_manager import Base
 
@@ -24,3 +26,20 @@ class StaticIAX(Base):
     category = Column(String(128), nullable=False)
     var_name = Column(String(128), nullable=False)
     var_val = Column(String(255))
+
+    @hybrid_property
+    def metric(self):
+        if self.var_metric == 0:
+            return None
+        return self.var_metric + 1
+
+    @metric.expression
+    def metric(cls):
+        return func.nullif(cls.var_metric, 0)
+
+    @metric.setter
+    def metric(self, value):
+        if value is None:
+            self.var_metric = 0
+        else:
+            self.var_metric = value + 1
