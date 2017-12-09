b72d839961984fb84ce69dc13068c585e7ceb73f
Francois Blackburn <fblackburn@wazo.community>
2017-12-09 08:50:40 -0500

    add iax_callnumberlimits dao

---
+++ xivo_dao/resources/iax_callnumberlimits/dao.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_dao.helpers.db_manager import daosession
+
+from .persistor import IAXCallNumberLimitsPersistor
+
+
+@daosession
+def find_all(session):
+    return IAXCallNumberLimitsPersistor(session).find_all()
+
+
+@daosession
+def edit_all(session, iax_callnumberlimits):
+    IAXCallNumberLimitsPersistor(session).edit_all(iax_callnumberlimits)
---
+++ xivo_dao/resources/iax_callnumberlimits/persistor.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_dao.alchemy.iaxcallnumberlimits import IAXCallNumberLimits
+
+
+class IAXCallNumberLimitsPersistor(object):
+
+    def __init__(self, session):
+        self.session = session
+
+    def find_all(self):
+        query = self.session.query(IAXCallNumberLimits)
+        return query.all()
+
+    def edit_all(self, iax_callnumberlimits):
+        self.session.query(IAXCallNumberLimits).delete()
+        self.session.add_all(iax_callnumberlimits)
+        self.session.flush()
