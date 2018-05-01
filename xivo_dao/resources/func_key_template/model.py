ef21b948a85b3239372687308e5ed7eea70fe6ca
Francois Blackburn <fblackburn@wazo.community>
2018-05-01 07:58:28 -0400

    func_key_template: remove UserTemplate model

--- xivo_dao/resources/func_key_template/model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
-
-from collections import namedtuple
-
-
-UserTemplate = namedtuple('UserTemplate', ['user_id', 'template_id'])
