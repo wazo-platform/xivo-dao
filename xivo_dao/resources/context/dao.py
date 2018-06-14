ecb0ab32c227869ce4e5dae817d387856575b1c3
Pascal Cadotte Michaud <pcm@wazo.io>
2018-06-14 08:48:21 -0400

    context: add the tenant_uuids param to the get_by method

--- xivo_dao/resources/context/dao.py
+++ xivo_dao/resources/context/dao.py
@@ -19,8 +19,8 @@ def get(session, context_id, tenant_uuids=None):
 
 
 @daosession
-def get_by(session, **criteria):
-    return ContextPersistor(session, context_search).get_by(criteria)
+def get_by(session, tenant_uuids=None, **criteria):
+    return ContextPersistor(session, context_search, tenant_uuids).get_by(criteria)
 
 
 @daosession
