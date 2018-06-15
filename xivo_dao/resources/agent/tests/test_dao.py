29aea10d0df8687bb365812bffef0c8c84c4930b
Francois Blackburn <fblackburn@wazo.community>
2018-06-15 14:48:36 -0400

    agent: remove unused "exists" method

--- xivo_dao/resources/agent/tests/test_dao.py
+++ xivo_dao/resources/agent/tests/test_dao.py
@@ -22,21 +22,6 @@ from xivo_dao.tests.test_dao import DAOTestCase
 from .. import dao as agent_dao
 
 
-class TestAgentExist(DAOTestCase):
-
-    def test_given_no_agent_then_returns_false(self):
-        result = agent_dao.exists(1)
-
-        assert_that(result, equal_to(False))
-
-    def test_given_agent_exists_then_return_true(self):
-        agent_row = self.add_agent()
-
-        result = agent_dao.exists(agent_row.id)
-
-        assert_that(result, equal_to(True))
-
-
 class TestFind(DAOTestCase):
 
     def test_find_no_agent(self):
