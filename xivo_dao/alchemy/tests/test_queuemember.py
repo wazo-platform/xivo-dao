bda37819f5f8d656d3fc2b273df792c6f7de31d2
Francois Blackburn <fblackburn@wazo.community>
2018-06-27 14:42:11 -0400

    coding style

--- xivo_dao/alchemy/tests/test_queuemember.py
+++ xivo_dao/alchemy/tests/test_queuemember.py
@@ -37,6 +37,28 @@ class TestAgent(DAOTestCase):
         assert_that(queue_member.agent, equal_to(agent))
 
 
+class TestExten(DAOTestCase):
+
+    def test_exten_with_local(self):
+        member = QueueMember(interface='Local/123@default')
+        assert_that(member.exten, equal_to('123'))
+
+    def test_exten_with_no_local(self):
+        member = QueueMember(interface='SIP/123')
+        assert_that(member.exten, is_(none()))
+
+
+class TestContext(DAOTestCase):
+
+    def test_context_with_local(self):
+        member = QueueMember(interface='Local/123@default')
+        assert_that(member.context, equal_to('default'))
+
+    def test_context_with_no_local(self):
+        member = QueueMember(interface='SIP/123')
+        assert_that(member.context, is_(none()))
+
+
 class TestDelete(DAOTestCase):
 
     def test_group_is_not_deleted(self):
@@ -68,22 +90,3 @@ class TestDelete(DAOTestCase):
 
         row = self.session.query(AgentFeatures).first()
         assert_that(row, equal_to(agent))
-
-
-class TestPropriety(DAOTestCase):
-
-    def test_exten_with_local(self):
-        member = QueueMember(interface='Local/123@default')
-        assert_that(member.exten, equal_to('123'))
-
-    def test_exten_with_no_local(self):
-        member = QueueMember(interface='SIP/123')
-        assert_that(member.exten, is_(none()))
-
-    def test_context_with_local(self):
-        member = QueueMember(interface='Local/123@default')
-        assert_that(member.context, equal_to('default'))
-
-    def test_context_with_no_local(self):
-        member = QueueMember(interface='SIP/123')
-        assert_that(member.context, is_(none()))
