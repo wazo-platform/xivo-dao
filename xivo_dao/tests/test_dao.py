e5e6af8de03050ce5552a09c5ff433cd51aea30e
Francois Blackburn <fblackburn@wazo.community>
2018-12-13 16:23:55 -0500

    remove user table
    reason: users has been migrated to wazo-auth

--- xivo_dao/tests/test_dao.py
+++ xivo_dao/tests/test_dao.py
@@ -96,7 +96,6 @@ from xivo_dao.alchemy.staticvoicemail import StaticVoicemail
 from xivo_dao.alchemy.switchboard import Switchboard
 from xivo_dao.alchemy.tenant import Tenant
 from xivo_dao.alchemy.trunkfeatures import TrunkFeatures
-from xivo_dao.alchemy.user import User
 from xivo_dao.alchemy.user_line import UserLine
 from xivo_dao.alchemy.usercustom import UserCustom as UserCustomSchema
 from xivo_dao.alchemy.userfeatures import UserFeatures
@@ -141,11 +140,6 @@ class ItemInserter(object):
 
     def __init__(self, session):
         self.session = session
-
-    def add_admin(self, **kwargs):
-        admin = User(**kwargs)
-        self.add_me(admin)
-        return admin
 
     def add_user_line_with_exten(self, **kwargs):
         kwargs.setdefault('firstname', 'unittest')
