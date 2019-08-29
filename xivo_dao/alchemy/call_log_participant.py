4a352d3e7e9e51dcc34bae6aef736dc5df19ec8c
Pascal Cadotte Michaud <pcm@wazo.io>
2019-08-29 14:23:16 -0400

    call_log_participant: add the peer_exten field
    this will add the field `peer_exten` to the call_log_participant which will
    contain the exten that was reached by a participant during this call

--- xivo_dao/alchemy/call_log_participant.py
+++ xivo_dao/alchemy/call_log_participant.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from sqlalchemy import Enum
@@ -8,10 +8,15 @@ from sqlalchemy.schema import Column
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.schema import Index
 from sqlalchemy.schema import PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
 from sqlalchemy.types import Integer, String
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.sql import case, select
 
 from xivo_dao.helpers.db_manager import Base
 from xivo_dao.helpers.uuid import new_uuid
+
+from .call_log import CallLog
 
 
 class CallLogParticipant(Base):
@@ -28,3 +33,32 @@ class CallLogParticipant(Base):
     line_id = Column(Integer)
     role = Column('role', Enum('source', 'destination', name='call_log_participant_role'), nullable=False)
     tags = Column(ARRAY(String(128)), nullable=False, server_default='{}')
+
+    call_log = relationship(
+        'CallLog',
+        primaryjoin='CallLog.id == CallLogParticipant.call_log_id',
+        uselist=False,
+    )
+
+    @hybrid_property
+    def peer_exten(self):
+        if self.role == 'source':
+            return self.call_log.requested_exten
+        else:
+            return self.call_log.source_exten
+
+    @peer_exten.expression
+    def peer_exten(cls):
+        return case(
+            [
+                (
+                    cls.role == 'source',
+                    select([CallLog.requested_exten]).where(
+                        cls.call_log_id == CallLog.id
+                    ).as_scalar()
+                )
+            ],
+            else_=select([CallLog.source_exten]).where(
+                cls.call_log_id == CallLog.id
+            ).as_scalar()
+        )
