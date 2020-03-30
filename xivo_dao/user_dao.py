b593ffc5fb0cb9cc0c0676c711b0bbf4a337ba22
Alexandre Fournier <afournier@wazo.io>
2020-03-30 10:29:44 -0400

    user_dao: add user uuid support

--- xivo_dao/user_dao.py
+++ xivo_dao/user_dao.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012-2016 Avencall
+# Copyright 2012-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
 
 from sqlalchemy import and_
-
 from xivo_dao.alchemy.extension import Extension
 from xivo_dao.alchemy.line_extension import LineExtension
 from xivo_dao.alchemy.linefeatures import LineFeatures
@@ -18,7 +17,10 @@ logger = logging.getLogger(__name__)
 
 @daosession
 def get(session, user_id):
-    result = session.query(UserFeatures).filter(UserFeatures.id == int(user_id)).first()
+    if isinstance(user_id, int):
+        result = session.query(UserFeatures).filter(UserFeatures.id == user_id).first()
+    else:
+        result = session.query(UserFeatures).filter(UserFeatures.uuid == user_id).first()
     if result is None:
         raise LookupError()
     return result
