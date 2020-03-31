41a71956886fb417031d28341fe9d321e48111e0
Pascal Cadotte Michaud <pcm@wazo.io>
2020-03-31 13:45:12 -0400

    transport options: remove the relationship to transports

--- xivo_dao/alchemy/pjsip_transport_option.py
+++ xivo_dao/alchemy/pjsip_transport_option.py
@@ -2,7 +2,6 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Column, PrimaryKeyConstraint, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.types import Integer, Text
@@ -25,5 +24,3 @@ class PJSIPTransportOption(Base):
         ForeignKey('pjsip_transport.uuid'),
         nullable=False,
     )
-
-    transport = relationship('PJSIPTransport')
