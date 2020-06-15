4357cd3aad139e6d30745327241247026408ffb3
Francois Blackburn <fblackburn@wazo.community>
2020-06-15 10:14:58 -0400

    add endpoint_sip_section unittests for options

--- xivo_dao/alchemy/tests/test_endpoint_sip_section.py
+++ xivo_dao/alchemy/tests/test_endpoint_sip_section.py
@@ -2,7 +2,12 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from hamcrest import assert_that, none
+from hamcrest import (
+    assert_that,
+    contains_inanyorder,
+    has_properties,
+    none,
+)
 from sqlalchemy.inspection import inspect
 
 from xivo_dao.tests.test_dao import DAOTestCase
@@ -24,3 +29,69 @@ class TestOptions(DAOTestCase):
         assert_that(inspect(section).deleted)
         option = self.session.query(EndpointSIPSectionOption).first()
         assert_that(option, none())
+
+    def test_create_an_option(self):
+        section = EndpointSIPSection(options=None)
+        self.session.add(section)
+        self.session.flush()
+
+        section.options = [['type', 'aor']]
+        self.session.flush()
+        self.session.expire_all()
+        assert_that(section.options, contains_inanyorder(
+            has_properties(key='type', value='aor'),
+        ))
+
+    def test_update_an_option(self):
+        section = EndpointSIPSection(options=[['type', 'aor'], ['old', 'value']])
+        self.session.add(section)
+        self.session.flush()
+
+        section.options = [['type', 'aor'], ['new', 'value']]
+        self.session.flush()
+        self.session.expire_all()
+
+        assert_that(section.options, contains_inanyorder(
+            has_properties(key='type', value='aor'),
+            has_properties(key='new', value='value'),
+        ))
+
+    def test_delete_an_option(self):
+        section = EndpointSIPSection(options=[['type', 'aor'], ['delete', 'value']])
+        self.session.add(section)
+        self.session.flush()
+
+        section.options = [['type', 'aor']]
+        self.session.flush()
+        self.session.expire_all()
+
+        assert_that(section.options, contains_inanyorder(
+            has_properties(key='type', value='aor'),
+        ))
+
+    def test_update_and_create_options(self):
+        section = EndpointSIPSection(options=[['update', 'old']])
+        self.session.add(section)
+        self.session.flush()
+
+        section.options = [['create', 'value'], ['update', 'new']]
+        self.session.flush()
+        self.session.expire_all()
+
+        assert_that(section.options, contains_inanyorder(
+            has_properties(key='create', value='value'),
+            has_properties(key='update', value='new'),
+        ))
+
+    def test_update_and_delete_options(self):
+        section = EndpointSIPSection(options=[['update', 'old'], ['delete', 'value']])
+        self.session.add(section)
+        self.session.flush()
+
+        section.options = [['update', 'new']]
+        self.session.flush()
+        self.session.expire_all()
+
+        assert_that(section.options, contains_inanyorder(
+            has_properties(key='update', value='new'),
+        ))
