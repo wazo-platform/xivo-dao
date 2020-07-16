22a82b135edb4ede62efd489da2174a85136963f
Pascal Cadotte Michaud <pcm@wazo.io>
2020-07-16 08:25:58 -0400

    asterisk conf dao: re-implement find_sip_user_settings
    this implementation returns a list of dictionnaries that map a the PJSIP config.
    It adds Wazo specific variables and some values that are comming from the
    endpoint's relations.

--- xivo_dao/asterisk_conf_dao.py
+++ xivo_dao/asterisk_conf_dao.py
@@ -9,6 +9,7 @@ from collections import (
     namedtuple,
 )
 
+from sqlalchemy.orm import joinedload
 from sqlalchemy.sql.expression import (
     and_,
     cast,
@@ -385,37 +386,141 @@ def find_sip_general_settings(session):
 
 @daosession
 def find_sip_user_settings(session):
-    # TODO(pc-m): I changed userSIP to EndpointSIP but did not change any logic
     pickup_members = find_pickup_members('sip')
-
     query = session.query(
-        EndpointSIP,
-        LineFeatures.context,
-        LineFeatures.application_uuid,
-        LineFeatures.endpoint_sip_uuid,
-        Extension.exten.label('number'),
-        UserFeatures.musiconhold.label('mohsuggest'),
-        UserFeatures.id.label('user_id'),
-        UserFeatures.uuid.label('uuid'),
-        (Voicemail.mailbox + '@' + Voicemail.context).label('mailbox'),
-    ).join(
         LineFeatures,
-    ).outerjoin(UserLine, and_(
-        UserLine.line_id == LineFeatures.id,
-        UserLine.main_user.is_(True),
-    )).outerjoin(
-        UserFeatures,
-    ).outerjoin(
-        Voicemail
-    ).outerjoin(
-        LineExtension
-    ).outerjoin(
-        Extension
+    ).options(
+        joinedload('endpoint_sip'),
+    ).options(
+        joinedload('user_lines').joinedload('user').joinedload('voicemail'),
+    ).options(
+        joinedload('line_extensions').joinedload('extension'),
+    ).filter(
+        LineFeatures.endpoint_sip_uuid.isnot(None),
     )
 
-    for row in query.all():
-        pickup_groups = pickup_members.get(row.EndpointSIP.uuid, {})
-        yield row, pickup_groups
+    lines = query.all()
+    context_mapping = {}
+    extension_mapping = {}
+    voicemail_mapping = defaultdict(list)
+    user_mapping = defaultdict(list)
+    raw_configs = {}
+    for line in lines:
+        raw_configs[line.endpoint_sip_uuid] = line.endpoint_sip
+        context_mapping[line.endpoint_sip_uuid] = line.context
+        if line.extensions:
+            extension_mapping[line.endpoint_sip_uuid] = line.extensions[0]
+        for user in line.users:
+            user_mapping[line.endpoint_sip_uuid].append(user)
+            if user.voicemail:
+                voicemail_mapping[line.endpoint_sip_uuid].append(user.voicemail)
+
+    def endpoint_to_dict(endpoint):
+        return {
+            'uuid': endpoint.uuid,
+            'name': endpoint.name,
+            'label': endpoint.label,
+            'aor_section_options': endpoint.aor_section_options,
+            'auth_section_options': endpoint.auth_section_options,
+            'endpoint_section_options': endpoint.endpoint_section_options,
+            'template': endpoint.template,
+            'asterisk_id': endpoint.asterisk_id,
+        }
+
+    def get_flat_config(endpoint):
+        if endpoint.uuid in flat_configs:
+            return flat_configs[endpoint.uuid]
+        else:
+            parents = [get_flat_config(parent) for parent in endpoint.templates]
+            base_config = endpoint_to_dict(endpoint)
+            context_name = context_mapping.get(endpoint.uuid)
+            if context_name:
+                base_config['endpoint_section_options'].append(['context', context_name])
+                base_config['endpoint_section_options'].append(
+                    ['set_var', 'TRANSFER_CONTEXT={}'.format(context_name)],
+                )
+            if endpoint.transport_uuid:
+                base_config['endpoint_section_options'].append(
+                    ['transport', endpoint.transport.name],
+                )
+            extension = extension_mapping.get(endpoint.uuid)
+            if extension:
+                base_config['endpoint_section_options'].append(
+                    ['set_var', 'PICKUPMARK={}%{}'.format(extension.exten, extension.context)]
+                )
+            voicemails = voicemail_mapping.get(endpoint.uuid, [])
+            mailboxes = []
+            for voicemail in voicemails:
+                mailboxes.append('{}@{}'.format(voicemail.number, voicemail.context))
+            if mailboxes:
+                base_config['aor_section_options'].append(
+                    ['mailboxes', ','.join(mailboxes)]
+                )
+            for user in user_mapping.get(endpoint.uuid, []):
+                base_config['endpoint_section_options'].extend([
+                    ['set_var', 'XIVO_USERID={}'.format(user.id)],
+                    ['set_var', 'XIVO_USERUUID={}'.format(user.uuid)],
+                    # TODO(pc-m): document WAZO_USER_UUID and deprecate the XIVO one
+                    # ['set_var', 'WAZO_USER_UUID={}'.format(user.uuid)],
+                ])
+            pickup_groups = pickup_members.get(endpoint.uuid, {})
+            named_pickup_groups = ','.join(str(id) for id in pickup_groups.get('pickupgroup', []))
+            if named_pickup_groups:
+                base_config['endpoint_section_options'].append(
+                    ['named_pickup_group', named_pickup_groups],
+                )
+            named_call_groups = ','.join(str(id) for id in pickup_groups.get('callgroup', []))
+            if named_call_groups:
+                base_config['endpoint_section_options'].append(
+                    ['named_call_group', named_call_groups],
+                )
+            builder = {}
+            for parent in parents + [base_config]:
+                for section in [
+                    'aor_section_options',
+                    'auth_section_options',
+                    'endpoint_section_options',
+                ]:
+                    builder.setdefault(section, [])
+                    parent_options = parent.get(section, [])
+                    for option in parent_options:
+                        builder[section].append(option)
+            for key, value in builder.get('endpoint_section_options', []):
+                if key == 'callerid':
+                    builder['endpoint_section_options'].append(
+                        ['set_var', 'XIVO_ORIGINAL_CALLER_ID={}'.format(value)]
+                    )
+                    break
+            builder['endpoint_section_options'].extend([
+                ['set_var', 'WAZO_CHANNEL_DIRECTION=from-wazo'],
+                ['set_var', 'WAZO_TENANT_UUID={}'.format(endpoint.tenant_uuid)],
+            ])
+            if builder['endpoint_section_options']:
+                builder['endpoint_section_options'].append(['type', 'endpoint'])
+            if builder['aor_section_options']:
+                builder['aor_section_options'].append(['type', 'aor'])
+                builder['endpoint_section_options'].append(['aors', endpoint.name])
+            if builder['auth_section_options']:
+                builder['auth_section_options'].append(['type', 'auth'])
+                builder['endpoint_section_options'].append(['auth', endpoint.name])
+            builder.update({
+                'uuid': base_config['uuid'],
+                'name': base_config['name'],
+                'label': base_config['label'],
+                'template': base_config['template'],
+                'asterisk_id': base_config['asterisk_id'],
+            })
+            if builder['template']:
+                flat_configs[builder['uuid']] = builder
+            return builder
+
+    # A flat_config is an endpoint config with all inherited fields merged into a single object
+    flat_configs = {}
+    for uuid, raw_config in raw_configs.items():
+        flat_config = get_flat_config(raw_config)
+        flat_configs[uuid] = flat_config
+
+    return [config for config in flat_configs.values() if config['template'] is False]
 
 
 @daosession
@@ -469,11 +574,11 @@ def find_pickup_members(session, protocol):
     ).filter(Pickup.commented == 0)
 
     if protocol == 'sip':
-        base_query = base_query.filter(LineFeatures.endpoint_sip_uuid != None)
+        base_query = base_query.filter(LineFeatures.endpoint_sip_uuid.isnot(None))
     elif protocol == 'sccp':
-        base_query = base_query.filter(LineFeatures.endpoint_sccp_id != None)
+        base_query = base_query.filter(LineFeatures.endpoint_sccp_id.isnot(None))
     elif protocol == 'custom':
-        base_query = base_query.filter(LineFeatures.endpoint_custom_id != None)
+        base_query = base_query.filter(LineFeatures.endpoint_custom_id.isnot(None))
 
     users = base_query.join(
         UserLine, UserLine.user_id == PickupMember.memberid,
