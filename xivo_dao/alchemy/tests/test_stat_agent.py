31987949437eb52eb1d1e6daa9922a43fa1260ba
Francois Blackburn <fblackburn@wazo.community>
2020-11-10 11:21:40 -0500

    stat_agent tests: remove unused expire_all

--- xivo_dao/alchemy/tests/test_stat_agent.py
+++ xivo_dao/alchemy/tests/test_stat_agent.py
@@ -14,22 +14,14 @@ from ..stat_agent import StatAgent
 class TestStatAgent(DAOTestCase):
     def test_agent_number(self):
         agent = self.add_stat_agent(name='Agent/1004')
-
-        self.session.expire_all()
-
         assert_that(agent.number, equal_to('1004'))
 
     def test_agent_number_empty(self):
         agent = self.add_stat_agent(name='')
-
-        self.session.expire_all()
-
         assert_that(agent.number, equal_to(None))
 
     def test_number_min(self):
         self.add_stat_agent(name='Agent/1004')
-
-        self.session.expire_all()
 
         result = self.session.query(
             func.min(StatAgent.number).label('min_number')
@@ -39,8 +31,6 @@ class TestStatAgent(DAOTestCase):
 
     def test_number_min_number_empty(self):
         self.add_stat_agent(name='not-prefixed')
-
-        self.session.expire_all()
 
         result = self.session.query(
             func.min(StatAgent.number).label('min_number')
