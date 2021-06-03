3a6a723119a6216969cf690024d3f60e2b0051c1
Pascal Cadotte Michaud <pcm@wazo.io>
2021-06-03 14:43:51 -0400

    switchboards: add moh

--- xivo_dao/alchemy/tests/test_switchboard.py
+++ xivo_dao/alchemy/tests/test_switchboard.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import (
     assert_that,
+    equal_to,
+    is_,
     none,
 )
 
-from xivo_dao.alchemy.dialaction import Dialaction
 from xivo_dao.tests.test_dao import DAOTestCase
+
+from ..dialaction import Dialaction
+from ..switchboard import Switchboard
 
 
 class TestDelete(DAOTestCase):
@@ -25,3 +29,37 @@ class TestDelete(DAOTestCase):
 
         row = self.session.query(Dialaction).first()
         assert_that(row, none())
+
+    def test_queue_music_on_hold(self):
+        moh_name = 'my-moh'
+        moh = self.add_moh(name=moh_name)
+
+        self.add_switchboard(queue_moh_uuid=moh.uuid)
+        self.session.flush()
+
+        row = self.session.query(Switchboard).first()
+        assert_that(row.queue_music_on_hold, equal_to(moh_name))
+
+    def test_waiting_room_music_on_hold(self):
+        moh_name = 'my-moh'
+        moh = self.add_moh(name=moh_name)
+
+        self.add_switchboard(hold_moh_uuid=moh.uuid)
+        self.session.flush()
+
+        row = self.session.query(Switchboard).first()
+        assert_that(row.waiting_room_music_on_hold, equal_to(moh_name))
+
+    def test_queue_music_on_hold_not_configured(self):
+        self.add_switchboard()
+        self.session.flush()
+
+        row = self.session.query(Switchboard).first()
+        assert_that(row.queue_music_on_hold, is_(none()))
+
+    def test_waiting_room_music_on_hold_not_configured(self):
+        self.add_switchboard()
+        self.session.flush()
+
+        row = self.session.query(Switchboard).first()
+        assert_that(row.waiting_room_music_on_hold, is_(none()))
