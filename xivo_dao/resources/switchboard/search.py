0461d444b0230ccfd3cc4535a95efcf10a953363
Julien Alie <jalie@wazo.io>
2021-08-26 14:28:38 -0400

    Fixes and formatting using Black
    * Added a test to validate the group_by clause
    * Formatted files using Black for consistent style

--- xivo_dao/resources/switchboard/search.py
+++ xivo_dao/resources/switchboard/search.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from sqlalchemy.sql import and_, cast
@@ -13,36 +13,47 @@ from xivo_dao.resources.utils.search import SearchSystem, SearchConfig
 config = SearchConfig(
     table=Switchboard,
     columns={
-        'name': Switchboard.name,
-        'exten': Incall.exten,
+        "name": Switchboard.name,
+        "exten": Incall.exten,
     },
     search=[
-        'name',
-        'exten',
+        "name",
+        "exten",
     ],
     sort=[
-        'name',
+        "name",
     ],
-    default_sort='name',
+    default_sort="name",
 )
 
 
 class SwitchboardSearchSystem(SearchSystem):
-
     def _search_on_extension(self, query):
         return (
-            query
-            .outerjoin(Dialaction, and_(Dialaction.action == 'switchboard',
-                                        Dialaction.actionarg1 == Switchboard.uuid))
-            .outerjoin(Incall, and_(Dialaction.category == 'incall',
-                                    Dialaction.categoryval == cast(Incall.id, String),
-                                    Incall.commented == 0))
+            query.outerjoin(
+                Dialaction,
+                and_(
+                    Dialaction.action == "switchboard",
+                    Dialaction.actionarg1 == Switchboard.uuid,
+                ),
+            )
+            .outerjoin(
+                Incall,
+                and_(
+                    Dialaction.category == "incall",
+                    Dialaction.categoryval == cast(Incall.id, String),
+                    Incall.commented == 0,
+                ),
+            )
             .group_by(Switchboard)
         )
 
     def search_from_query(self, query, parameters):
         query = self._search_on_extension(query)
-        return super(SwitchboardSearchSystem, self,).search_from_query(query, parameters)
+        return super(
+            SwitchboardSearchSystem,
+            self,
+        ).search_from_query(query, parameters)
 
 
 switchboard_search = SwitchboardSearchSystem(config)
--- xivo_dao/resources/switchboard/tests/tests_dao.py
+++ xivo_dao/resources/switchboard/tests/tests_dao.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
@@ -28,14 +28,12 @@ from .. import dao as switchboard_dao
 
 
 class TestSearch(DAOTestCase):
-
     def assert_search_returns_result(self, search_result, **parameters):
         result = switchboard_dao.search(**parameters)
         assert_that(result, equal_to(search_result))
 
 
 class TestSimpleSearch(TestSearch):
-
     def test_given_no_switchboards_then_returns_no_empty_result(self):
         expected = SearchResult(0, [])
 
@@ -50,8 +48,8 @@ class TestSimpleSearch(TestSearch):
     def test_search_multi_tenant(self):
         tenant = self.add_tenant()
 
-        switchboard1 = self.add_switchboard(name='a')
-        switchboard2 = self.add_switchboard(name='b', tenant_uuid=tenant.uuid)
+        switchboard1 = self.add_switchboard(name="a")
+        switchboard2 = self.add_switchboard(name="b", tenant_uuid=tenant.uuid)
 
         expected = SearchResult(2, [switchboard1, switchboard2])
         tenants = [tenant.uuid, self.default_tenant.uuid]
@@ -64,51 +62,81 @@ class TestSimpleSearch(TestSearch):
     def test_search_by_extension(self):
         switchboard = self.add_switchboard()
         incall = self.add_incall()
-        self.add_extension(type='incall',
-                           typeval=str(incall.id),
-                           exten="1000")
-        self.add_dialaction(category='incall',
-                            categoryval=str(incall.id),
-                            action='switchboard',
-                            actionarg1=switchboard.uuid)
+        self.add_extension(type="incall", typeval=str(incall.id), exten="1000")
+        self.add_dialaction(
+            category="incall",
+            categoryval=str(incall.id),
+            action="switchboard",
+            actionarg1=switchboard.uuid,
+        )
 
         expected = SearchResult(1, [switchboard])
         self.assert_search_returns_result(expected, search="1000")
+        self.assert_search_returns_result(expected, exten="1000")
+
+    def test_search_by_extension_with_multiple_incalls(self):
+        switchboard = self.add_switchboard()
+        incall1 = self.add_incall()
+        incall2 = self.add_incall()
+        self.add_extension(type="incall", typeval=str(incall1.id), exten="1000")
+        self.add_extension(type="incall", typeval=str(incall2.id), exten="1001")
+        self.add_dialaction(
+            category="incall",
+            categoryval=str(incall1.id),
+            action="switchboard",
+            actionarg1=switchboard.uuid,
+        )
+        self.add_dialaction(
+            category="incall",
+            categoryval=str(incall2.id),
+            action="switchboard",
+            actionarg1=switchboard.uuid,
+        )
+
+        expected = SearchResult(1, [switchboard])
+        self.assert_search_returns_result(expected, search="100")
 
 
 class TestSearchGivenMultipleSwitchboards(TestSearch):
-
     def setUp(self):
         super(TestSearch, self).setUp()
-        self.switchboard1 = self.add_switchboard(name='Ashton')
-        self.switchboard2 = self.add_switchboard(name='Beaugarton')
-        self.switchboard3 = self.add_switchboard(name='Casa')
-        self.switchboard4 = self.add_switchboard(name='Dunkin')
+        self.switchboard1 = self.add_switchboard(name="Ashton")
+        self.switchboard2 = self.add_switchboard(name="Beaugarton")
+        self.switchboard3 = self.add_switchboard(name="Casa")
+        self.switchboard4 = self.add_switchboard(name="Dunkin")
 
     def test_when_searching_then_returns_one_result(self):
         expected = SearchResult(1, [self.switchboard2])
 
-        self.assert_search_returns_result(expected, search='eau')
+        self.assert_search_returns_result(expected, search="eau")
 
     def test_when_sorting_then_returns_result_in_ascending_order(self):
-        expected = SearchResult(4, [
-            self.switchboard1,
-            self.switchboard2,
-            self.switchboard3,
-            self.switchboard4,
-        ])
+        expected = SearchResult(
+            4,
+            [
+                self.switchboard1,
+                self.switchboard2,
+                self.switchboard3,
+                self.switchboard4,
+            ],
+        )
 
-        self.assert_search_returns_result(expected, order='name')
+        self.assert_search_returns_result(expected, order="name")
 
-    def test_when_sorting_in_descending_order_then_returns_results_in_descending_order(self):
-        expected = SearchResult(4, [
-            self.switchboard4,
-            self.switchboard3,
-            self.switchboard2,
-            self.switchboard1,
-        ])
+    def test_when_sorting_in_descending_order_then_returns_results_in_descending_order(
+        self,
+    ):
+        expected = SearchResult(
+            4,
+            [
+                self.switchboard4,
+                self.switchboard3,
+                self.switchboard2,
+                self.switchboard1,
+            ],
+        )
 
-        self.assert_search_returns_result(expected, order='name', direction='desc')
+        self.assert_search_returns_result(expected, order="name", direction="desc")
 
     def test_when_limiting_then_returns_right_name_of_items(self):
         expected = SearchResult(4, [self.switchboard1])
@@ -116,7 +144,9 @@ class TestSearchGivenMultipleSwitchboards(TestSearch):
         self.assert_search_returns_result(expected, limit=1)
 
     def test_when_offset_then_returns_right_name_of_items(self):
-        expected = SearchResult(4, [self.switchboard2, self.switchboard3, self.switchboard4])
+        expected = SearchResult(
+            4, [self.switchboard2, self.switchboard3, self.switchboard4]
+        )
 
         self.assert_search_returns_result(expected, offset=1)
 
@@ -125,16 +155,15 @@ class TestSearchGivenMultipleSwitchboards(TestSearch):
 
         self.assert_search_returns_result(
             expected,
-            search='a',
-            order='name',
-            direction='desc',
+            search="a",
+            order="name",
+            direction="desc",
             offset=1,
             limit=1,
         )
 
 
 class TestGet(DAOTestCase):
-
     def test_get_no_switchboard(self):
         self.assertRaises(NotFoundError, switchboard_dao.get, 42)
 
@@ -149,31 +178,34 @@ class TestGet(DAOTestCase):
         tenant = self.add_tenant()
 
         switchboard_row = self.add_switchboard(tenant_uuid=tenant.uuid)
-        switchboard = switchboard_dao.get(switchboard_row.uuid, tenant_uuids=[tenant.uuid])
+        switchboard = switchboard_dao.get(
+            switchboard_row.uuid, tenant_uuids=[tenant.uuid]
+        )
         assert_that(switchboard, equal_to(switchboard_row))
 
         switchboard_row = self.add_switchboard()
         self.assertRaises(
             NotFoundError,
-            switchboard_dao.get, switchboard_row.uuid, tenant_uuids=[tenant.uuid],
+            switchboard_dao.get,
+            switchboard_row.uuid,
+            tenant_uuids=[tenant.uuid],
         )
 
 
 class TestGetBy(DAOTestCase):
-
     def test_given_column_does_not_exist_then_error_raised(self):
         self.assertRaises(InputError, switchboard_dao.get_by, invalid=42)
 
     def test_get_by_name(self):
-        switchboard_row = self.add_switchboard(name='switchboard')
+        switchboard_row = self.add_switchboard(name="switchboard")
 
-        switchboard = switchboard_dao.get_by(name='switchboard')
+        switchboard = switchboard_dao.get_by(name="switchboard")
 
         assert_that(switchboard, equal_to(switchboard_row))
-        assert_that(switchboard.name, equal_to('switchboard'))
+        assert_that(switchboard.name, equal_to("switchboard"))
 
     def test_given_switchboard_does_not_exist_then_raises_error(self):
-        self.assertRaises(NotFoundError, switchboard_dao.get_by, name='not-found')
+        self.assertRaises(NotFoundError, switchboard_dao.get_by, name="not-found")
 
     def test_get_by_multi_tenant(self):
         tenant = self.add_tenant()
@@ -181,16 +213,19 @@ class TestGetBy(DAOTestCase):
         switchboard_row = self.add_switchboard()
         self.assertRaises(
             NotFoundError,
-            switchboard_dao.get_by, uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid],
+            switchboard_dao.get_by,
+            uuid=switchboard_row.uuid,
+            tenant_uuids=[tenant.uuid],
         )
 
         switchboard_row = self.add_switchboard(tenant_uuid=tenant.uuid)
-        switchboard = switchboard_dao.get_by(uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid])
+        switchboard = switchboard_dao.get_by(
+            uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid]
+        )
         assert_that(switchboard, equal_to(switchboard_row))
 
 
 class TestFind(DAOTestCase):
-
     def test_find_no_switchboard(self):
         result = switchboard_dao.find(42)
 
@@ -210,25 +245,26 @@ class TestFind(DAOTestCase):
         result = switchboard_dao.find(switchboard.uuid, tenant_uuids=[tenant.uuid])
         assert_that(result, equal_to(switchboard))
 
-        result = switchboard_dao.find(switchboard.uuid, tenant_uuids=[self.default_tenant.uuid])
+        result = switchboard_dao.find(
+            switchboard.uuid, tenant_uuids=[self.default_tenant.uuid]
+        )
         assert_that(result, none())
 
 
 class TestFindBy(DAOTestCase):
-
     def test_given_column_does_not_exist_then_error_raised(self):
         self.assertRaises(InputError, switchboard_dao.find_by, invalid=42)
 
     def test_find_by_name(self):
-        switchboard_row = self.add_switchboard(name='switchboard')
+        switchboard_row = self.add_switchboard(name="switchboard")
 
-        switchboard = switchboard_dao.find_by(name='switchboard')
+        switchboard = switchboard_dao.find_by(name="switchboard")
 
         assert_that(switchboard, equal_to(switchboard_row))
-        assert_that(switchboard.name, equal_to('switchboard'))
+        assert_that(switchboard.name, equal_to("switchboard"))
 
     def test_given_switchboard_does_not_exist_then_returns_null(self):
-        switchboard = switchboard_dao.find_by(name='not-found')
+        switchboard = switchboard_dao.find_by(name="not-found")
 
         assert_that(switchboard, none())
 
@@ -236,77 +272,87 @@ class TestFindBy(DAOTestCase):
         tenant = self.add_tenant()
 
         switchboard_row = self.add_switchboard()
-        switchboard = switchboard_dao.find_by(uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid])
+        switchboard = switchboard_dao.find_by(
+            uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid]
+        )
         assert_that(switchboard, none())
 
         switchboard_row = self.add_switchboard(tenant_uuid=tenant.uuid)
-        switchboard = switchboard_dao.find_by(uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid])
+        switchboard = switchboard_dao.find_by(
+            uuid=switchboard_row.uuid, tenant_uuids=[tenant.uuid]
+        )
         assert_that(switchboard, equal_to(switchboard_row))
 
 
 class TestFindAllBy(DAOTestCase):
-
     def test_find_all_by_no_switchboards(self):
-        result = switchboard_dao.find_all_by(name='switchboard')
+        result = switchboard_dao.find_all_by(name="switchboard")
 
         assert_that(result, contains())
 
     def test_find_all_by_native_column(self):
-        switchboard1 = self.add_switchboard(name='switchboard')
-        switchboard2 = self.add_switchboard(name='switchboard')
+        switchboard1 = self.add_switchboard(name="switchboard")
+        switchboard2 = self.add_switchboard(name="switchboard")
 
-        switchboards = switchboard_dao.find_all_by(name='switchboard')
+        switchboards = switchboard_dao.find_all_by(name="switchboard")
 
-        assert_that(switchboards, has_items(
-            has_property('uuid', switchboard1.uuid),
-            has_property('uuid', switchboard2.uuid),
-        ))
+        assert_that(
+            switchboards,
+            has_items(
+                has_property("uuid", switchboard1.uuid),
+                has_property("uuid", switchboard2.uuid),
+            ),
+        )
 
     def test_find_all_by_multi_tenant(self):
         tenant = self.add_tenant()
 
-        switchboard1 = self.add_switchboard(name='name', tenant_uuid=tenant.uuid)
-        switchboard2 = self.add_switchboard(name='name')
+        switchboard1 = self.add_switchboard(name="name", tenant_uuid=tenant.uuid)
+        switchboard2 = self.add_switchboard(name="name")
 
         tenants = [tenant.uuid, self.default_tenant.uuid]
-        switchboards = switchboard_dao.find_all_by(name='name', tenant_uuids=tenants)
+        switchboards = switchboard_dao.find_all_by(name="name", tenant_uuids=tenants)
         assert_that(switchboards, has_items(switchboard1, switchboard2))
 
         tenants = [tenant.uuid]
-        switchboards = switchboard_dao.find_all_by(name='name', tenant_uuids=tenants)
-        assert_that(switchboards, all_of(has_items(switchboard1), not_(has_items(switchboard2))))
+        switchboards = switchboard_dao.find_all_by(name="name", tenant_uuids=tenants)
+        assert_that(
+            switchboards, all_of(has_items(switchboard1), not_(has_items(switchboard2)))
+        )
 
 
 class TestCreate(DAOTestCase):
-
     def test_create_minimal_fields(self):
-        switchboard = Switchboard(name='switchboard', tenant_uuid=self.default_tenant.uuid)
+        switchboard = Switchboard(
+            name="switchboard", tenant_uuid=self.default_tenant.uuid
+        )
 
         switchboard = switchboard_dao.create(switchboard)
 
         self.session.expire_all()
         assert_that(inspect(switchboard).persistent)
-        assert_that(switchboard, has_properties(
-            uuid=is_not(none()),
-            tenant_uuid=self.default_tenant.uuid,
-            name='switchboard',
-        ))
+        assert_that(
+            switchboard,
+            has_properties(
+                uuid=is_not(none()),
+                tenant_uuid=self.default_tenant.uuid,
+                name="switchboard",
+            ),
+        )
 
 
 class TestEdit(DAOTestCase):
-
     def test_edit_all_fields(self):
-        switchboard = self.add_switchboard(name='switchboard')
+        switchboard = self.add_switchboard(name="switchboard")
 
-        switchboard.name = 'other_switchboard'
+        switchboard.name = "other_switchboard"
         switchboard_dao.edit(switchboard)
 
         self.session.expire_all()
-        assert_that(switchboard, has_properties(name='other_switchboard'))
+        assert_that(switchboard, has_properties(name="other_switchboard"))
 
 
 class TestDelete(DAOTestCase):
-
     def test_delete(self):
         switchboard = self.add_switchboard()
 
