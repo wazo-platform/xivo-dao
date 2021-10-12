85b8a568a5bbaa8ffc3cec4c7327ed6c1858924a
Pascal Cadotte Michaud <pcm@wazo.io>
2021-10-12 11:21:00 -0400

    call_pickup: refactor: use the BasePersistor

--- xivo_dao/resources/call_pickup/persistor.py
+++ xivo_dao/resources/call_pickup/persistor.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-from sqlalchemy import text
 
 from xivo_dao.alchemy.pickup import Pickup as CallPickup
 from xivo_dao.helpers import errors
-from xivo_dao.resources.utils.search import SearchResult, CriteriaBuilderMixin
+from xivo_dao.helpers.persistor import BasePersistor
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
-class CallPickupPersistor(CriteriaBuilderMixin):
+class CallPickupPersistor(CriteriaBuilderMixin, BasePersistor):
 
     _search_table = CallPickup
 
     def __init__(self, session, call_pickup_search, tenant_uuids=None):
         self.session = session
-        self.call_pickup_search = call_pickup_search
+        self.search_system = call_pickup_search
         self.tenant_uuids = tenant_uuids
-
-    def find_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.first()
 
     def _find_query(self, criteria):
         query = self.session.query(CallPickup)
@@ -28,43 +23,19 @@ class CallPickupPersistor(CriteriaBuilderMixin):
         return self.build_criteria(query, criteria)
 
     def get_by(self, criteria):
-        call_pickup = self.find_by(criteria)
-        if not call_pickup:
+        model = self.find_by(criteria)
+        if not model:
             raise errors.not_found('CallPickup', **criteria)
-        return call_pickup
+        return model
 
-    def _filter_tenant_uuid(self, query):
-        if self.tenant_uuids is None:
-            return query
-
-        if not self.tenant_uuids:
-            return query.filter(text('false'))
-
-        return query.filter(CallPickup.tenant_uuid.in_(self.tenant_uuids))
-
-    def find_all_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.all()
-
-    def search(self, parameters):
-        query = self.session.query(self.call_pickup_search.config.table)
-        query = self._filter_tenant_uuid(query)
-        rows, total = self.call_pickup_search.search_from_query(query, parameters)
-        return SearchResult(total, rows)
+    def _search_query(self):
+        return self.session.query(self.search_system.config.table)
 
     def create(self, call_pickup):
         self._fill_default_values(call_pickup)
         self.session.add(call_pickup)
         self.session.flush()
         return call_pickup
-
-    def edit(self, call_pickup):
-        self.session.add(call_pickup)
-        self.session.flush()
-
-    def delete(self, call_pickup):
-        self.session.delete(call_pickup)
-        self.session.flush()
 
     def _fill_default_values(self, call_pickup):
         last_id = self.session.query(CallPickup.id).order_by(CallPickup.id.desc()).limit(1).scalar()
