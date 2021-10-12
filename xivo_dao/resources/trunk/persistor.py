29e0b1563692b1fe8d594cd61ea794a43541d5c0
Pascal Cadotte Michaud <pcm@wazo.io>
2021-10-12 13:50:04 -0400

    trunk: refactor: use the BasePersistor

--- xivo_dao/resources/trunk/persistor.py
+++ xivo_dao/resources/trunk/persistor.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-from sqlalchemy import text
 
 from xivo_dao.alchemy.endpoint_sip import EndpointSIP
 from xivo_dao.alchemy.trunkfeatures import TrunkFeatures as Trunk
@@ -11,21 +9,18 @@ from xivo_dao.alchemy.useriax import UserIAX
 from xivo_dao.alchemy.usercustom import UserCustom
 
 from xivo_dao.helpers import errors
-from xivo_dao.resources.utils.search import SearchResult, CriteriaBuilderMixin
+from xivo_dao.helpers.persistor import BasePersistor
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
-class TrunkPersistor(CriteriaBuilderMixin):
+class TrunkPersistor(CriteriaBuilderMixin, BasePersistor):
 
     _search_table = Trunk
 
     def __init__(self, session, trunk_search, tenant_uuids=None):
         self.session = session
-        self.trunk_search = trunk_search
+        self.search_system = trunk_search
         self.tenant_uuids = tenant_uuids
-
-    def find_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.first()
 
     def _find_query(self, criteria):
         query = self.session.query(Trunk)
@@ -33,29 +28,13 @@ class TrunkPersistor(CriteriaBuilderMixin):
         return self.build_criteria(query, criteria)
 
     def get_by(self, criteria):
-        trunk = self.find_by(criteria)
-        if not trunk:
+        model = self.find_by(criteria)
+        if not model:
             raise errors.not_found('Trunk', **criteria)
-        return trunk
+        return model
 
-    def find_all_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.all()
-
-    def search(self, parameters):
-        query = self.session.query(self.trunk_search.config.table)
-        query = self._filter_tenant_uuid(query)
-        rows, total = self.trunk_search.search_from_query(query, parameters)
-        return SearchResult(total, rows)
-
-    def create(self, trunk):
-        self.session.add(trunk)
-        self.session.flush()
-        return trunk
-
-    def edit(self, trunk):
-        self.session.add(trunk)
-        self.session.flush()
+    def _search_query(self):
+        return self.session.query(self.search_system.config.table)
 
     def delete(self, trunk):
         if trunk.endpoint_sip_uuid:
@@ -82,15 +61,6 @@ class TrunkPersistor(CriteriaBuilderMixin):
 
         self.session.delete(trunk)
         self.session.flush()
-
-    def _filter_tenant_uuid(self, query):
-        if self.tenant_uuids is None:
-            return query
-
-        if not self.tenant_uuids:
-            return query.filter(text('false'))
-
-        return query.filter(Trunk.tenant_uuid.in_(self.tenant_uuids))
 
     def associate_endpoint_sip(self, trunk, endpoint):
         if trunk.protocol not in ('sip', None):
