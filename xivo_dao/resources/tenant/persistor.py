a845633fba583ba46b9740a8dc357e9fff570f94
Pascal Cadotte Michaud <pcm@wazo.io>
2021-10-12 13:48:25 -0400

    tenant: refactor: use the BasePersistor

--- xivo_dao/resources/tenant/persistor.py
+++ xivo_dao/resources/tenant/persistor.py
@@ -1,42 +1,25 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from sqlalchemy import text
 
-from xivo_dao.helpers import errors
+from xivo_dao.helpers.persistor import BasePersistor
 from xivo_dao.alchemy.tenant import Tenant
-from xivo_dao.resources.utils.search import CriteriaBuilderMixin, SearchResult
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
-class Persistor(CriteriaBuilderMixin):
+class Persistor(CriteriaBuilderMixin, BasePersistor):
 
     _search_table = Tenant
 
     def __init__(self, session, search, tenant_uuids=None):
         self.session = session
-        self.resource_search = search
+        self.search_system = search
         self.tenant_uuids = tenant_uuids
 
-    def find_all_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.all()
-
-    def find_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.first()
-
-    def get_by(self, criteria):
-        resource = self.find_by(criteria)
-        if not resource:
-            raise errors.not_found('Tenant', **criteria)
-        return resource
-
-    def search(self, parameters):
-        query = self.session.query(Tenant)
-        query = self._filter_tenant_uuid(query)
-        rows, total = self.resource_search.search_from_query(query, parameters)
-        return SearchResult(total, rows)
+    def _search_query(self):
+        return self.session.query(self.search_system.config.table)
 
     def _find_query(self, criteria):
         query = self.session.query(Tenant)
