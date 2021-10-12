c5c8c9a9efdca0924d1777aa4675f3c5834ba9af
Pascal Cadotte Michaud <pcm@wazo.io>
2021-10-12 11:26:16 -0400

    context: refactor: use the BasePersistor

--- xivo_dao/resources/context/persistor.py
+++ xivo_dao/resources/context/persistor.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_dao.alchemy.context import Context
 from xivo_dao.alchemy.contextmember import ContextMember
 
-from xivo_dao.helpers import errors
-from xivo_dao.resources.utils.search import SearchResult, CriteriaBuilderMixin
+from xivo_dao.helpers.persistor import BasePersistor
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
-class ContextPersistor(CriteriaBuilderMixin):
+class ContextPersistor(CriteriaBuilderMixin, BasePersistor):
 
     _search_table = Context
 
     def __init__(self, session, context_search, tenant_uuids=None):
         self.session = session
-        self.context_search = context_search
+        self.search_system = context_search
         self.tenant_uuids = tenant_uuids
-
-    def find_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.first()
 
     def _find_query(self, criteria):
         query = self.session.query(Context)
@@ -28,28 +24,8 @@ class ContextPersistor(CriteriaBuilderMixin):
             query = query.filter(Context.tenant_uuid.in_(self.tenant_uuids))
         return self.build_criteria(query, criteria)
 
-    def get_by(self, criteria):
-        context = self.find_by(criteria)
-        if not context:
-            raise errors.not_found('Context', **criteria)
-        return context
-
-    def find_all_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.all()
-
-    def search(self, parameters):
-        query = self.session.query(Context)
-        if self.tenant_uuids is not None:
-            query = query.filter(Context.tenant_uuid.in_(self.tenant_uuids))
-
-        rows, total = self.context_search.search_from_query(query, parameters)
-        return SearchResult(total, rows)
-
-    def create(self, context):
-        self.session.add(context)
-        self.session.flush()
-        return context
+    def _search_query(self):
+        return self.session.query(Context)
 
     def edit(self, context):
         self.session.add(context)
