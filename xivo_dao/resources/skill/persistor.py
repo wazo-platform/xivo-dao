c671b0bc0e5176e9118fc8fcca02c829899c80ed
Pascal Cadotte Michaud <pcm@wazo.io>
2021-10-12 13:41:40 -0400

    skill: refactor: use the BasePersistor

--- xivo_dao/resources/skill/persistor.py
+++ xivo_dao/resources/skill/persistor.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-from sqlalchemy import text
 
 from xivo_dao.alchemy.queueskill import QueueSkill
 from xivo_dao.alchemy.queueskillcat import QueueSkillCat
 
 from xivo_dao.helpers import errors
-from xivo_dao.resources.utils.search import SearchResult, CriteriaBuilderMixin
+from xivo_dao.helpers.persistor import BasePersistor
+from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
-class SkillPersistor(CriteriaBuilderMixin):
+class SkillPersistor(CriteriaBuilderMixin, BasePersistor):
 
     _search_table = QueueSkill
 
     def __init__(self, session, skill_search, tenant_uuids=None):
         self.session = session
-        self.skill_search = skill_search
+        self.search_system = skill_search
         self.tenant_uuids = tenant_uuids
-
-    def find_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.first()
 
     def _find_query(self, criteria):
         query = self.session.query(QueueSkill)
@@ -30,20 +25,13 @@ class SkillPersistor(CriteriaBuilderMixin):
         return self.build_criteria(query, criteria)
 
     def get_by(self, criteria):
-        skill = self.find_by(criteria)
-        if not skill:
+        model = self.find_by(criteria)
+        if not model:
             raise errors.not_found('Skill', **criteria)
-        return skill
+        return model
 
-    def find_all_by(self, criteria):
-        query = self._find_query(criteria)
-        return query.all()
-
-    def search(self, parameters):
-        query = self.session.query(self.skill_search.config.table)
-        query = self._filter_tenant_uuid(query)
-        rows, total = self.skill_search.search_from_query(query, parameters)
-        return SearchResult(total, rows)
+    def _search_query(self):
+        return self.session.query(self.search_system.config.table)
 
     def create(self, skill):
         self._set_or_create_category(skill)
@@ -68,16 +56,3 @@ class SkillPersistor(CriteriaBuilderMixin):
         if not category:
             category = QueueSkillCat(name=skill._category)
         skill.queue_skill_cat = category
-
-    def _filter_tenant_uuid(self, query):
-        if self.tenant_uuids is None:
-            return query
-
-        if not self.tenant_uuids:
-            return query.filter(text('false'))
-
-        return query.filter(QueueSkill.tenant_uuid.in_(self.tenant_uuids))
-
-    def delete(self, skill):
-        self.session.delete(skill)
-        self.session.flush()
