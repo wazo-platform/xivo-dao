01c2ff2bf75b3cf417bc0475035ea5d598c0c9c2
Alexandre Fournier <afournier@wazo.io>
2021-11-01 21:32:51 -0400

    moh: remove unique constraint
    Why: using the name as a slug removes the need for a unique name.

--- xivo_dao/alchemy/moh.py
+++ xivo_dao/alchemy/moh.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from sqlalchemy.schema import Column, ForeignKey, PrimaryKeyConstraint, UniqueConstraint
+from sqlalchemy.schema import Column, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.types import String, Text
 
 from xivo_dao.helpers.db_manager import Base
@@ -14,7 +14,6 @@ class MOH(Base):
     __tablename__ = 'moh'
     __table_args__ = (
         PrimaryKeyConstraint('uuid'),
-        UniqueConstraint('name'),
     )
 
     uuid = Column(String(38), nullable=False, default=new_uuid)
