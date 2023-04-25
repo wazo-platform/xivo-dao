710280a932efc456dff8973aba02bbabe591e53d
Charles Langlois <charles.langlois@wazo.io>
2023-04-25 15:53:02 -0400

    test_stat_dao_extended: updated fill_leaveempty_calls sql function, added test cases

--- xivo_dao/tests/helpers/fill_leaveempty_calls.sql
+++ xivo_dao/tests/helpers/fill_leaveempty_calls.sql
@@ -2,6 +2,23 @@ DROP FUNCTION IF EXISTS "fill_leaveempty_calls" (timestamptz, timestamptz);
 CREATE OR REPLACE FUNCTION "fill_leaveempty_calls" (period_start timestamptz, period_end timestamptz)
 RETURNS void AS
 $$
+WITH 
+leave_call as (
+    select main.id, main.callid, main.time as leave_time, main.queuename, 
+        (select time from queue_log 
+        where callid = main.callid and queuename = main.queuename 
+        and time < main.time and event = 'ENTERQUEUE' 
+        order by time desc limit 1) as enter_time, 
+        stat_queue.id as stat_queue_id 
+    from queue_log as main 
+    left join stat_queue on stat_queue.name = main.queuename
+    where event='LEAVEEMPTY'
+),
+leave_call_in_range as (
+    select *
+    from leave_call
+    where enter_time BETWEEN $1 AND $2 
+)
 INSERT INTO stat_call_on_queue (callid, time, waittime, stat_queue_id, status)
 SELECT
 callid,
@@ -9,15 +26,7 @@ enter_time as time,
 EXTRACT(EPOCH FROM (leave_time - enter_time))::INTEGER as waittime,
 stat_queue_id,
 'leaveempty' AS status
-FROM (SELECT
-    time AS enter_time,
-    (select time from queue_log where callid=main.callid AND event='LEAVEEMPTY' LIMIT 1) AS leave_time,
-    callid,
-    (SELECT id FROM stat_queue WHERE name=queuename) AS stat_queue_id
-  FROM queue_log AS main
-  WHERE callid IN (SELECT callid FROM queue_log WHERE event = 'LEAVEEMPTY')
-        AND event = 'ENTERQUEUE'
-        AND time BETWEEN $1 AND $2) AS first;
+FROM leave_call_in_range;
 $$
 LANGUAGE SQL;
-
+ 
