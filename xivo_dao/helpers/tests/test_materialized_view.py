169ffc4761f92372431738463975eb86ce0d8bb8
Julien Alie <jalie@wazo.io>
2023-05-09 10:11:06 -0400

    refactor views using sqlalchemy-utils
    Why:
    
    * Simplifies maintainability and future expansion by
    using a library instead of custom implementation

--- xivo_dao/helpers/tests/test_materialized_view.py
+++ xivo_dao/helpers/tests/test_materialized_view.py
@@ -1,6 +1,7 @@
-# Copyright 2021-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from functools import partial
 from hamcrest import (
     assert_that,
     instance_of,
@@ -8,9 +9,8 @@ from hamcrest import (
     not_,
     raises,
     equal_to,
-    has_items,
 )
-from sqlalchemy import select, Table, Column, Integer
+from sqlalchemy import select, Table
 from sqlalchemy.exc import InvalidRequestError
 
 from xivo_dao.tests.test_dao import DAOTestCase
@@ -21,8 +21,7 @@ from xivo_dao.helpers.db_views import MaterializedView, create_materialized_view
 class TestCreateMaterializedView(DAOTestCase):
     def test_create_materialized_view(self):
         result = create_materialized_view('test', select([1]))
-        assert_that(result, instance_of(tuple))
-        assert_that(result, has_items(instance_of(Table), instance_of(list)))
+        assert_that(result, instance_of(Table))
 
     def test_create_materialized_view_with_no_name(self):
         assert_that(
@@ -42,14 +41,6 @@ class TestCreateMaterializedView(DAOTestCase):
             raises(ValueError)
         )
 
-    def test_create_materialized_view_with_invalid_dependencies(self):
-        assert_that(
-            calling(create_materialized_view).with_args(
-                'testview', select([1]), dependencies=[object]
-            ),
-            raises(ValueError)
-        )
-
     def test_create_materialized_view_with_invalid_indexes(self):
         assert_that(
             calling(create_materialized_view).with_args(
@@ -58,72 +49,12 @@ class TestCreateMaterializedView(DAOTestCase):
             raises(ValueError)
         )
 
-
-class TestMaterializedView(DAOTestCase):
-    def _create_view_class(
-        self, func, name=None, selectable=None, dependencies=None, indexes=None
-    ):
-        class _(MaterializedView):
-            __view__ = (
-                func(name, selectable, dependencies=dependencies, indexes=indexes)
-                if callable(func)
-                else func
-            )
-
-        return _
-
-    def test_view_init_table(self):
-        assert_that(
-            calling(self._create_view_class).with_args(
-                create_materialized_view, name='test', selectable=select([1])
-            ),
-            not_(raises(InvalidRequestError)),
-        )
-
-    def test_view_class_creation(self):
-        view = self._create_view_class(create_materialized_view, "view", select([1]))
-        assert_that(issubclass(view, MaterializedView), equal_to(True))
-
-    def test_view_init_with_none(self):
-        assert_that(
-            calling(self._create_view_class).with_args(None),
-            raises(InvalidRequestError),
-        )
-
-    def test_view_without_helper_function(self):
-        def some_func(name, selectable, dependencies=None, indexes=None):
-            return "bad return"
-
-        assert_that(
-            calling(self._create_view_class).with_args(some_func, "view", select([1])),
-            raises(InvalidRequestError)
-        )
-
-    def test_view_init_with_invalid_view(self):
-        assert_that(
-            calling(self._create_view_class).with_args((None, None)),
-            raises(InvalidRequestError),
-        )
-
-    def test_view_init_without_view_attribute(self):
-        def _create_class():
+    def test_create_materialized_view_class(self):
+        def create_view():
             class _(MaterializedView):
-                pass
+                __table__ = create_materialized_view('testview', select([1]))
 
             return _
 
-        assert_that(calling(_create_class), raises(InvalidRequestError))
-
-    def test_view_dependencies_event_correctly_bound(self):
-        class MockModel(Base):
-            __tablename__ = 'mock_model'
-            id = Column(Integer, primary_key=True)
-
-        view = self._create_view_class(
-            create_materialized_view, "view", select([1]), dependencies=[MockModel]
-        )
-        assert_that(view.autorefresh, equal_to(True))
-
-    def test_view_dependencies_no_event_bound(self):
-        view = self._create_view_class(create_materialized_view, "view", select([1]))
-        assert_that(view.autorefresh, equal_to(False))
+        view = create_view()
+        assert_that(issubclass(view, MaterializedView), equal_to(True))
