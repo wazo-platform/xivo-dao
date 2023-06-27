c7549dd7e8846ef5c8188e24a05db90c2e75459e
Julien Alie <jalie@wazo.io>
2023-06-27 09:15:43 -0400

    blackify user's view.py

--- xivo_dao/resources/user/view.py
+++ xivo_dao/resources/user/view.py
@@ -66,69 +66,76 @@ class UserView(View):
 
 
 class DirectoryView(View):
-
     def query(self, session):
-        query = (session.query(User.id.label('id'),
-                               User.uuid.label('uuid'),
-                               UserLine.line_id.label('line_id'),
-                               User.agentid.label('agent_id'),
-                               User.firstname.label('firstname'),
-                               func.nullif(User.lastname, '').label('lastname'),
-                               func.nullif(User.email, '').label('email'),
-                               func.nullif(User.mobilephonenumber, '').label('mobile_phone_number'),
-                               Voicemail.mailbox.label('voicemail_number'),
-                               func.nullif(User.userfield, '').label('userfield'),
-                               func.nullif(User.description, '').label('description'),
-                               Extension.exten.label('exten'),
-                               Extension.context.label('context')))
+        query = session.query(
+            User.id.label('id'),
+            User.uuid.label('uuid'),
+            UserLine.line_id.label('line_id'),
+            User.agentid.label('agent_id'),
+            User.firstname.label('firstname'),
+            func.nullif(User.lastname, '').label('lastname'),
+            func.nullif(User.email, '').label('email'),
+            func.nullif(User.mobilephonenumber, '').label('mobile_phone_number'),
+            Voicemail.mailbox.label('voicemail_number'),
+            func.nullif(User.userfield, '').label('userfield'),
+            func.nullif(User.description, '').label('description'),
+            Extension.exten.label('exten'),
+            Extension.context.label('context'),
+        )
         return query
 
     def convert(self, row):
-        return UserDirectory(id=row.id,
-                             uuid=row.uuid,
-                             line_id=row.line_id,
-                             agent_id=row.agent_id,
-                             firstname=row.firstname,
-                             lastname=row.lastname,
-                             email=row.email,
-                             mobile_phone_number=row.mobile_phone_number,
-                             voicemail_number=row.voicemail_number,
-                             exten=row.exten,
-                             userfield=row.userfield,
-                             description=row.description,
-                             context=row.context)
+        return UserDirectory(
+            id=row.id,
+            uuid=row.uuid,
+            line_id=row.line_id,
+            agent_id=row.agent_id,
+            firstname=row.firstname,
+            lastname=row.lastname,
+            email=row.email,
+            mobile_phone_number=row.mobile_phone_number,
+            voicemail_number=row.voicemail_number,
+            exten=row.exten,
+            userfield=row.userfield,
+            description=row.description,
+            context=row.context,
+        )
 
 
 class SummaryView(View):
     def query(self, session):
-        query = (session.query(User.id.label('id'),
-                               User.uuid.label('uuid'),
-                               User.firstname.label('firstname'),
-                               func.nullif(User.lastname, '').label('lastname'),
-                               func.nullif(User.email, '').label('email'),
-                               User.enabled.label('enabled'),
-                               case([
-                                   (Line.endpoint_custom_id.is_(None), Line.provisioning_code)
-                               ],
-                                   else_=None).label('provisioning_code'),
-                               Line.protocol.label('protocol'),
-                               Extension.exten.label('extension'),
-                               Extension.context.label('context')))
+        query = session.query(
+            User.id.label('id'),
+            User.uuid.label('uuid'),
+            User.firstname.label('firstname'),
+            func.nullif(User.lastname, '').label('lastname'),
+            func.nullif(User.email, '').label('email'),
+            User.enabled.label('enabled'),
+            case(
+                [(Line.endpoint_custom_id.is_(None), Line.provisioning_code)],
+                else_=None,
+            ).label('provisioning_code'),
+            Line.protocol.label('protocol'),
+            Extension.exten.label('extension'),
+            Extension.context.label('context'),
+        )
         return query
 
     def convert(self, row):
-        return UserSummary(id=row.id,
-                           uuid=row.uuid,
-                           firstname=row.firstname,
-                           lastname=row.lastname,
-                           email=row.email,
-                           enabled=row.enabled,
-                           provisioning_code=row.provisioning_code,
-                           protocol=row.protocol,
-                           extension=row.extension,
-                           context=row.context)
+        return UserSummary(
+            id=row.id,
+            uuid=row.uuid,
+            firstname=row.firstname,
+            lastname=row.lastname,
+            email=row.email,
+            enabled=row.enabled,
+            provisioning_code=row.provisioning_code,
+            protocol=row.protocol,
+            extension=row.extension,
+            context=row.context,
+        )
 
 
-user_view = ViewSelector(default=UserView(),
-                         directory=DirectoryView(),
-                         summary=SummaryView())
+user_view = ViewSelector(
+    default=UserView(), directory=DirectoryView(), summary=SummaryView()
+)
