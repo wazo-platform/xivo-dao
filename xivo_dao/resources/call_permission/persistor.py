c80ef0eaee9201703d072ce258bc1b93762b3eb1
Julien Alie <jalie@wazo.io>
2023-07-03 15:52:47 -0400

    move relationship loading to query instead of model

--- xivo_dao/resources/call_permission/persistor.py
+++ xivo_dao/resources/call_permission/persistor.py
@@ -1,5 +1,7 @@
-# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from sqlalchemy.orm import joinedload
 
 from xivo_dao.alchemy.rightcall import RightCall as CallPermission
 from xivo_dao.helpers import errors
@@ -8,7 +10,6 @@ from xivo_dao.resources.utils.search import CriteriaBuilderMixin
 
 
 class CallPermissionPersistor(CriteriaBuilderMixin, BasePersistor):
-
     _search_table = CallPermission
 
     def __init__(self, session, call_permission_search, tenant_uuids=None):
@@ -17,7 +18,7 @@ class CallPermissionPersistor(CriteriaBuilderMixin, BasePersistor):
         self.tenant_uuids = tenant_uuids
 
     def _find_query(self, criteria):
-        query = self.session.query(CallPermission)
+        query = self._query_with_relationships()
         query = self._filter_tenant_uuid(query)
         return self.build_criteria(query, criteria)
 
@@ -28,4 +29,18 @@ class CallPermissionPersistor(CriteriaBuilderMixin, BasePersistor):
         return model
 
     def _search_query(self):
-        return self.session.query(self.search_system.config.table)
+        return self._query_with_relationships()
+
+    def _query_with_relationships(self):
+        return self.session.query(CallPermission).options(
+            joinedload(CallPermission.rightcall_groups)
+            .joinedload('group')
+            .load_only('uuid', 'id', 'name'),
+            joinedload(CallPermission.rightcall_users)
+            .joinedload('user')
+            .load_only('uuid', 'firstname', 'webi_lastname'),
+            joinedload(CallPermission.rightcall_outcalls)
+            .joinedload('outcall')
+            .load_only('id', 'name'),
+            joinedload(CallPermission.rightcallextens),
+        )
