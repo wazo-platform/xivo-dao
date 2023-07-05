d63e2c815f53aead980a8a636ca119b3ef11673e
Alexandre Fournier <afournier@wazo.io>
2023-07-05 16:27:39 -0400

    outcall: add label column

--- xivo_dao/resources/outcall/search.py
+++ xivo_dao/resources/outcall/search.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_dao.alchemy.outcall import Outcall
@@ -10,6 +10,7 @@ config = SearchConfig(table=Outcall,
                       columns={'id': Outcall.id,
                                'description': Outcall.description,
                                'name': Outcall.name,
+                               'label': Outcall.label,
                                'preprocess_subroutine': Outcall.preprocess_subroutine},
                       default_sort='id')
 
