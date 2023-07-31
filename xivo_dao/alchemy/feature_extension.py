4f38ae0276fc596e5de240f5d294732d06f1013c
billou <nabil.benyoussef@wazo.io>
2023-07-31 21:32:35 +0200

    Change columns for features extensions

--- xivo_dao/alchemy/feature_extension.py
+++ xivo_dao/alchemy/feature_extension.py
@@ -1,11 +1,13 @@
 # Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from sqlalchemy import text
+from sqlalchemy.dialects.postgresql import UUID
+
 from sqlalchemy.schema import Column, UniqueConstraint, Index, PrimaryKeyConstraint
 from sqlalchemy.types import String, Boolean
 
-from xivo_dao.helpers.db_manager import Base, IntAsString
-from xivo_dao.helpers.uuid import new_uuid
+from xivo_dao.helpers.db_manager import Base
 
 
 class FeatureExtension(Base):
@@ -18,10 +20,10 @@ class FeatureExtension(Base):
         Index('feature_extension__idx__uuid', 'uuid'),
     )
 
-    uuid = Column(String(38), nullable=False, default=new_uuid)
+    uuid = Column(UUID(as_uuid=True), server_default=text('uuid_generate_v4()'))
     enabled = Column(Boolean, nullable=False, server_default='true')
-    exten = Column(String(40), nullable=False, server_default='')
-    feature = Column(IntAsString(255), nullable=False, server_default='')
+    exten = Column(String(40), nullable=False)
+    feature = Column(String(255), nullable=False)
 
     def is_pattern(self):
         return self.exten.startswith('_')
