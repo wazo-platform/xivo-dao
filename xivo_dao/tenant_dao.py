b7cb46c0dc7438ebb248c0e6e1d7689bac742169
billou <nabil.benyoussef@wazo.io>
2023-11-07 16:57:58 +0100

    Improve concurrent get/create on tenant

--- xivo_dao/tenant_dao.py
+++ xivo_dao/tenant_dao.py
@@ -1,5 +1,7 @@
-# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from sqlalchemy.exc import IntegrityError
 
 from xivo_dao.alchemy.tenant import Tenant
 from xivo_dao.helpers.db_manager import daosession
@@ -7,16 +9,21 @@ from xivo_dao.helpers.db_manager import daosession
 
 @daosession
 def find_or_create_tenant(session, tenant_uuid):
-    result = session.query(Tenant).get(tenant_uuid)
-    if result:
-        return result
+    tenant = session.query(Tenant).get(tenant_uuid)
+
+    if tenant:
+        return tenant
 
     tenant = Tenant(uuid=tenant_uuid)
-    session.add(tenant)
-    session.flush()
 
+    session.begin_nested()
+    try:
+        session.add(tenant)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        tenant = session.query(Tenant).get(tenant_uuid)
     return tenant
-
 
 @daosession
 def find(session):
