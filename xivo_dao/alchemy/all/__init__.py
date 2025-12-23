cfb89daab071ea9b3b4fba3291999c323bfe23fb
Julien Alie <jalie@wazo.io>
2025-12-23 10:39:28 -0500

    remove materialized_view (endpoint_sip and helpers)

--- xivo_dao/alchemy/all/__init__.py
+++ xivo_dao/alchemy/all/__init__.py
@@ -29,7 +29,6 @@ from xivo_dao.alchemy.dhcp import Dhcp
 from xivo_dao.alchemy.dialaction import Dialaction
 from xivo_dao.alchemy.dialpattern import DialPattern
 from xivo_dao.alchemy.endpoint_sip import EndpointSIP, EndpointSIPTemplate
-from xivo_dao.alchemy.endpoint_sip_options_view import EndpointSIPOptionsView
 from xivo_dao.alchemy.endpoint_sip_section import EndpointSIPSection
 from xivo_dao.alchemy.endpoint_sip_section_option import EndpointSIPSectionOption
 from xivo_dao.alchemy.extension import Extension
@@ -152,7 +151,6 @@ __all__ = [
     "EndpointSIP",
     "EndpointSIPSection",
     "EndpointSIPSectionOption",
-    "EndpointSIPOptionsView",
     "EndpointSIPTemplate",
     "Extension",
     "ExternalApp",
