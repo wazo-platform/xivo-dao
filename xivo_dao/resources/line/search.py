e5741af56f09c0bc71ec6e8f3b61629ad2258182
Julien Alie <jalie@wazo.io>
2026-01-09 22:26:24 -0500

    lines: optmize search query when searching for callerid
    this commit modifies the search system to only fetch the recursive
    endpoint_sip query once which cuts processing time by half

--- xivo_dao/resources/line/search.py
+++ xivo_dao/resources/line/search.py
@@ -1,12 +1,31 @@
-# Copyright 2015-2025 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from sqlalchemy import sql
 from sqlalchemy.sql.elements import and_
 
+from xivo_dao.alchemy.endpoint_sip import EndpointSIP
 from xivo_dao.alchemy.extension import Extension
 from xivo_dao.alchemy.line_extension import LineExtension
 from xivo_dao.alchemy.linefeatures import LineFeatures
 from xivo_dao.resources.utils.search import SearchConfig, SearchSystem
+
+# NOTE(jalie): The following variables replaces hybrid_properties for
+# searching by callerid's name and num because they trigger 2 full scans
+# This approach queries the callerid only once and then apply transformation
+# for searching
+callerid_subquery = (
+    EndpointSIP.build_sip_option_subquery('callerid', 'endpoint').select().cte()
+)
+
+cid_name = LineFeatures.build_caller_id_expression(
+    sql.func.substring(callerid_subquery.c.value, '"([^"]+)"'), 'cid_name'
+)
+
+cid_num = LineFeatures.build_caller_id_expression(
+    sql.func.substring(callerid_subquery.c.value, '<([0-9A-Z]+)?>'), 'cid_name'
+)
+
 
 config = SearchConfig(
     table=LineFeatures,
@@ -19,8 +38,8 @@ config = SearchConfig(
         'protocol': LineFeatures.protocol,
         'device_id': LineFeatures.device,
         'name': LineFeatures.name,
-        'caller_id_name': LineFeatures.caller_id_name,
-        'caller_id_num': LineFeatures.caller_id_num,
+        'caller_id_name': cid_name,
+        'caller_id_num': cid_num,
         'exten': Extension.exten,
     },
     default_sort='name',
@@ -30,7 +49,16 @@ config = SearchConfig(
 class LineSearchSystem(SearchSystem):
     def search_from_query(self, query, parameters):
         query = self._search_on_extension(query)
+        query = self._search_on_endpoints(query)
         return super().search_from_query(query, parameters)
+
+    def _search_on_endpoints(self, query):
+        query = query.outerjoin(
+            callerid_subquery,
+            callerid_subquery.c.root == LineFeatures.endpoint_sip_uuid,
+        )
+
+        return query
 
     def _search_on_extension(self, query):
         return query.outerjoin(
