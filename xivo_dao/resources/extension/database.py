acb4e57db739ad7a14b53a81bf5caf3659b1cd8f
Alexandre Fournier <afournier@wazo.io>
2026-03-23 13:44:51 -0400

    feature: remove *34/calllistening
    Why: dangerous on a multi-tenant environment

--- xivo_dao/resources/extension/database.py
+++ xivo_dao/resources/extension/database.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2025 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import annotations
@@ -33,7 +33,6 @@ class ServiceExtensionConverter:
         "vmuserpurge",
         "phonestatus",
         "recsnd",
-        "calllistening",
         "directoryaccess",
         "fwdundoall",
         "pickup",
